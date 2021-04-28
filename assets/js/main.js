652e01777694173fe037ba0c5a1c5703270fb9b5
Ricard Garcia <imricardgarcia@gmail.com>
2021-04-28 16:26:49 +0200

    Added first lines for posts request

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,22 @@
 /*                                   TESTING                                  */
 /* -------------------------------------------------------------------------- */
 console.log("Loaded javaScript file");
+
+/* -------------------------------------------------------------------------- */
+/*                              GLOBAL VARIABLES                              */
+/* -------------------------------------------------------------------------- */
+// Responese
+let allPosts;
+
+// All posts
+var settings = {
+  url: "https://jsonplaceholder.typicode.com/posts",
+  method: "GET",
+  timeout: 0,
+  headers: {},
+};
+
+// Getting all posts
+$.ajax(settings).done(function (response) {
+  console.log(response);
+});
