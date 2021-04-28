488294b485cd7f3c8f7418727b2c9ccf330129b8
haykbit <hpetrosyan.projects@gmail.com>
2021-04-28 22:13:42 +0200

    Finished data collect functions, modal added

--- jQuery.js
+++ jQuery.js
@@ -1,8 +1,10 @@
 let urlPosts = 'https://jsonplaceholder.typicode.com/posts/';
 let urlUsers = 'https://jsonplaceholder.typicode.com/users';
 
-console.log($('.container')[0]);
 
+var globalUsers = [];
+
+//____________ FIRST POST _______________//
 var settings = {
     "url": urlPosts,
     "method": "GET",
@@ -14,13 +16,40 @@ var settings = {
 
 $.ajax(settings).done(function (response) {
     response.forEach(data => {
+        $('#postText').text(data.body);
         $('#postTitle').text(data.title);
     });
 });
 
-function getPost(id, url) {
+//____________ GET USERS _______________//
+function getUsers(callback) {
+
     var settings = {
-        "url": url,
+        "url": urlUsers,
+        "method": "GET",
+        "timeout": 0,
+    };
+
+    $.ajax(settings)
+        .then(function (response) {
+            callback(response);
+        })
+
+}
+getUsers(function (response) {
+    for (let i = 0; i < response.length; i++) {
+        globalUsers.push(response[i]);
+    }
+    for (let i = 2; i < 101; i++) {
+        getPost(i, createPost);
+    }
+    console.log(globalUsers);
+});
+
+//____________ GET POSTS _______________//
+function getPost(id,callback) {
+    var settings = {
+        "url": urlPosts,
         "method": "GET",
         "data": {
             id: id,
@@ -28,40 +57,34 @@ function getPost(id, url) {
         "timeout": 0,
     };
 
-    $.ajax(settings).done(function (response) {
-        response.forEach(data => {
-            $('#titlePost' + id).text(data.title);
-            $('#bodyPost' + id).text(data.body);
-        });
-    });
+    $.ajax(settings)
+        .then(function (response) {
+            response.forEach(data => {
+                callback(id,data);
+            })
+        })
 }
 
-function createPosts(id) {
-    var post = $('<div class="col-md-6"><div class="postBox row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative"> <div class="col p-4 d-flex flex-column position-static"> <strong class="d-inline-block mb-2 text-success">Jose Lara</strong><h3 class="mb-3 titlePost" id="titlePost' + id + '">Featured post</h3><p class="card-text mb-auto bodyPost" id="bodyPost' + id + '">This is a wider card with supporting text below as a naturallead-in to additional content.</p><a href="#" class="stretched-link">Continue reading</a></div></div></div>')
+//____________ CREATE POST _______________//
+function createPost(id,data) {
+    var post = $('<div class="col-md-6" data-id="'+id+'"><div class="postBox row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative"> <div class="col p-4 d-flex flex-column position-static"> <strong class="d-inline-block mb-2 text-success" id="userName' + id + '">Jose Lara</strong><h3 class="mb-3 titlePost" id="titlePost' + id + '">Featured post</h3><p class="card-text mb-auto bodyPost" id="bodyPost' + id + '">This is a wider card with supporting text below as a naturallead-in to additional content.</p> <button type="button" class="btn btn-primary modalBtn" data-toggle="modal" data-target="#exampleModal">See the post</button></div></div></div>')
     $('#postDiv').append(post);
-    getPost(id, 'https://jsonplaceholder.typicode.com/posts/');
-}
-for (let i = 1; i < 99; i++) {
-    createPosts(i);
-}
+    $('#titlePost' + id).text(data.title);
+    $('#bodyPost' + id).text(data.body);
 
-function getComments(id) {
-    var settings = {
-        "url": 'https://jsonplaceholder.typicode.com/posts/' + id + '/comments',
-        "method": "GET",
-        "timeout": 0,
-    };
-
-    $.ajax(settings).done(function (response) {
-        response.forEach(data => {
-            console.log(data);
+    $(".postBox").hover(
+        function () {
+            $(this).addClass('shadow-lg').css('cursor', 'pointer');
+        }, function () {
+            $(this).removeClass('shadow-lg');
         });
-    });
+
+    for (let i = 0; i < globalUsers.length; i++) {
+        if (data.userId == 1) {
+            $('#userName').text(globalUsers[0].name);
+        }
+        if (globalUsers[i].id == data.userId) {
+            $('#userName' + id).text(globalUsers[i].name);
+        }
+    }
 }
-//____________ STYLES _______________//
-$(".postBox").hover(
-    function () {
-        $(this).addClass('shadow-lg').css('cursor', 'pointer');
-    }, function () {
-        $(this).removeClass('shadow-lg');
-    });
