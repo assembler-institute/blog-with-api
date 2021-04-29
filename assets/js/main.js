62fed4d383e43133174eaecf2c65d5a53c17a8bc
Mathias Fortuna <mathiasfortuna@hotmail.com>
2021-04-29 16:54:26 +0200

    Finished comments collapsable

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,13 @@
-/* 
+// CONSTANTS
+const baseURL = `http://localhost:3000/`;
+
+/*
 To run on document load
 */
 $(function () {
   getPosts(0, 10);
 });
+
 // TODO first start load animation
 /*
 This function gets posts between input numbers from API and displays them in homepage
@@ -11,7 +15,7 @@ This function gets posts between input numbers from API and displays them in hom
 function getPosts(from, to) {
   var postsJSON = [];
   const settings = {
-    url: `https://jsonplaceholder.typicode.com/posts?_start=${from}&_limit=${to}`,
+    url: `${baseURL}posts?_start=${from}&_limit=${to}`,
     method: "GET",
     timeout: 0,
   };
@@ -19,6 +23,7 @@ function getPosts(from, to) {
     populatePosts(response);
   });
 }
+
 /* 
 This function inserts in DOM all elements from an array of objects
 */
@@ -36,6 +41,7 @@ function populatePosts(postsJSON) {
                 .attr("postId", postsJSON[index].id)
                 .attr("data-bs-toggle", "modal")
                 .attr("data-bs-target", "#postModal")
+                .attr("role", "button")
                 .append(
                   $("<h5>").addClass("card-title").text(postsJSON[index].title)
                 )
@@ -48,34 +54,122 @@ function populatePosts(postsJSON) {
 /* 
 OPEN MODAL
 */
-
 $("#postModal").on("show.bs.modal", function (event) {
   const postId = $(event.relatedTarget).attr("postId");
   const settings = {
-    url: `https://jsonplaceholder.typicode.com/posts/${postId}`,
+    url: `${baseURL}posts/${postId}`,
     method: "GET",
     timeout: 0,
   };
 
   $.ajax(settings).done(function (response) {
-    console.log(response);
+    renderUserPostModal(response.userId);
+    injectCommentCount(response.id);
     $("#postModalLabel").text(response.title);
     $("#postModalText").text(response.body);
     $("#postModal").attr("userId", response.userId);
-    $("#postModal").attr("postId", response.id);
-    renderUserPostModal(response.userId);
+    $("#openComments").attr("postId", response.id);
+    $("#openComments").on("click", openComments); //TODO add variables
   });
 });
 
+// Event post modal close
+$("#postModal").on("hidden.bs.modal", function () {
+  $("#openComments").off();
+  if ($("#commentsCollapse").hasClass("show")) {
+    closeComments();
+  }
+  $(".modal-content").css("display", "none");
+  $("#spinner-container").css("display", "inline-block");
+});
+
+/* 
+This function adds the information of the user that made the post
+*/
 function renderUserPostModal(userId) {
   var settings = {
-    url: `https://jsonplaceholder.typicode.com/users/${userId}`,
+    url: `${baseURL}users/${userId}`,
     method: "GET",
     timeout: 0,
   };
-
   $.ajax(settings).done(function (response) {
     $("#username").text(response.username);
     $("#userEmail").text(response.email);
+  });
+}
+
+/* 
+This function injects the number of comments in the comments button
+*/
+function injectCommentCount(postId) {
+  var settings = {
+    url: `${baseURL}comments?postId=${postId}`,
+    method: "GET",
+    timeout: 0,
+  };
+  $.ajax(settings).done(function (response) {
+    $("#commentsCount").text(response.length);
+    $("#spinner-container").fadeOut(200, () => $(".modal-content").fadeIn(200));
+  });
+}
+
+/* 
+This function opens and injects info into the collapsable comments section
+*/
+function openComments() {
+  $("#openComments").off();
+  $("#openComments").on("click", closeComments);
+  const postId = $(this).attr("postId");
+  var settings = {
+    url: `${baseURL}comments?postId=${postId}`,
+    method: "GET",
+    timeout: 0,
+  };
+  $("#commentsCollapse").empty();
+  $.ajax(settings).done(function (response) {
+    $(response).each((index, element) => {
+      $("#commentsCollapse").append(
+        $("<div>")
+          .addClass("card")
+          .append(
+            $("<div>")
+              .addClass("card-body")
+              .append(
+                $("<h5>").addClass("card-title").text(element.name),
+                $("<h6>")
+                  .addClass("card-subtitle mb-2 text-muted")
+                  .text(element.email),
+                $("<p>").addClass("card-text").text(element.body)
+              )
+          )
+      );
+    });
+    toggleCollapseComments();
+    $("#commentsCount").addClass("d-none");
+    $("#openComments span:last-child").text("Close comments");
+  });
+}
+
+/* 
+This function closes the collapsable comments section
+*/
+function closeComments() {
+  toggleCollapseComments();
+  $("#openComments").off();
+  $("#openComments").on("click", openComments);
+  $("#commentsCount").removeClass("d-none");
+  $("#openComments span:last-child").text("Comments");
+}
+
+/* 
+This function collapses or opens the comments section
+*/
+function toggleCollapseComments() {
+  // Collapse
+  var collapseElementList = [].slice.call(
+    document.querySelectorAll("#commentsCollapse")
+  );
+  var collapseList = collapseElementList.map(function (collapseEl) {
+    return new bootstrap.Collapse(collapseEl);
   });
 }
