50168a0118e6c7cc557504f13d8ef3dc6b511ecb
JonCroatan <43881219+jonCroatanUto@users.noreply.github.com>
2021-04-29 18:03:08 +0200

    alomst finish

--- assets/data.js
+++ assets/data.js
@@ -1,61 +1,98 @@
 
 
-$('#usBut').on('click',testingDataUs);
-$('#posBut').on('click',testingDataPo);
-$('#comBut').on('click',testingDataCom);
-
-function testingDataUs(){
-    return axios.get('https://jsonplaceholder.typicode.com/users')
+$('#usBut').on('click',DataUser);
+//$('#posBut').on('click',DataPost);
+//$('#comBut').on('click',DataComent);
+let url='http://localhost:3000/'
+function DataUser(){
+    return axios.get(url+'users')
+    
     .then(
-        function dataUs(responseUs){
+        function dataU(responseUs){
+            
             let count=0;
-            $.each( responseUs.data, function( index, elem ){
+            $.each( responseUs.data, function( index, elemUs ){
                 
-                if($('.getText').val()==elem.name){
-                  $('.name').text('thats your id '+`${elem.id}`)  
-                  $('.usName').text('thats your username '+`${elem.username}`)  
-                  $('.usEmail').text('thats your email '+` ${elem.email}`)  
-                    
-                    count+=1
+            if($('#floatingInput').val()==elemUs.name){
+                    $('#post').empty();
+                 /*$('.name').text('thats your id '+`${elemUs.id}`)  
+                  $('.usName').text('thats your username '+`${elemUs.username}`)  
+                  $('.usEmail').text('thats your email '+` ${elemUs.email}`)*/
+                  DataPost(elemUs.id,elemUs.name);
+                  count+=1
                 }
                 
             })
             if(count==0){
-                $('.userId').text('el usuario que mencionas no esta registrado')
+                $('#post').text('el usuario que mencionas no esta registrado')
             }
             
         }
         
     )
+    
         
 }
-function testingDataPo(){
-    return axios.get('https://jsonplaceholder.typicode.com/posts')
+function DataPost(userId,userName){
+    
+    return axios.get(url+'post')
     .then(
-        function dataPos(responsePos){
-            $.each( responsePos.data, function( index, elem ){
-                console.log(elem)
-                $('.post').append('<div></div>');
-                $('.post div:last-child').attr('id','posCont'+index);
-
-                $(`#posCont${index}`).append('<p>'+`${elem.id}`+'</p>')
+        function dataP(responsePos){
+            let cnt=0;
+            $.each( responsePos.data, function( index, elemPos ){
                 
-                $(`#posCont${index}`).append('<p>'+`${elem.title}`+'</p>')
-                $(`#posCont${index}`).append('<p>'+`${elem.body}`+'</p>')
-                    
+                
+                if(userId==elemPos.userId){
+                    cnt=cnt+1;
+                $('#post').append('<div></div>');
+                $('#post div:last-child').attr('id','posCont'+index);
+                $('#post div:last-child').attr('class','col');
+
+                //$(`#posCont${index}`).append('<p>'+`${elemPos.id}`+'</p>')
+                $(`#posCont${index}`).append('<p>'+`<span class='user'>${userName}</span>  :`+'</p>')
+                $(`#posCont${index}`).append('<p>'+`<span class='tit'>Title ${cnt} :<br></span> ${elemPos.title}`+'</p>')
+                
+                //$(`#posCont${index}`).append('<p>'+`${elemPos.body}`+'</p>')
+                $(`#posCont${index}`).append('<button  class="sendButP">Full info</button>')
+                $(`#posCont${index} button.sendButP`).on('click',function(){postData(this,elemPos.id,elemPos.body)})
+                
+
+                }   
             })
 
         }
     )
 }
-function testingDataCom(){
 
+function postData(event,post_id,post_body){
     
-    return axios.get('https://jsonplaceholder.typicode.com/posts/1/comments')
+/*if(x>0){
+    $('.idpost').remove()
+    $('.bodypost').remove()
+}else{*/
+    $(event).parent().append(`<p class='idpost'>${post_id}</p>`);
+    $(event).parent().append(`<p class='bodypost'>${post_body}</p>`);
+    $(event).parent().append(`<button class="sendButC">See post coments </button>`);
+    $('button.sendButC').on('click',function(){DataComent(post_id)})
+    $(event).parent().css('transition','2s');
+    x=+1;
+//}
+
+
+}
+
+function DataComent(idpost){
+   
+   
+    return axios.get(url+'coments')
     .then(
         function dataCom(responseCom){
-            $.each( responseCom.data, function( index, elem ){
-                
+            $.each( responseCom.data, function( index, elemCom ){
+                if(idpost==elemCom.postId){
+                    console.log(elemCom.name)
+                    console.log(elemCom.email)
+                    console.log(elemCom.body)
+                }
             })
         }
     )
