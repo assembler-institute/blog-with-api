673a624deacf338cd9a208471bb21e86024d521e
Jordi <aejordi@gmail.com>
2021-04-29 16:52:09 +0200

    S01 - Main project finished

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,39 @@
+function userData(userId) {
+  $.get(
+    "https://jsonplaceholder.typicode.com/users/" + userId,
+    function (userData) {
+      $("#authorInfo").text(userData.name + " - " + userData.email);
+    }
+  );
+}
+
+function commentsData(currentPostId) {
+  $.get(
+    "https://jsonplaceholder.typicode.com/posts/" + currentPostId + "/comments",
+    function (commentsData) {
+      $(".accordion-body").empty();
+      $(commentsData).each(function (index, comment) {
+        let divComment = $("<div>").addClass(
+          "comment container rounded-3 p-3 mb-2"
+        );
+        $("<h6>")
+          .addClass("comment-title")
+          .text(comment.name)
+          .appendTo(divComment);
+        $("<p>")
+          .addClass("comment-body")
+          .text(comment.body)
+          .appendTo(divComment);
+        $("<p>")
+          .addClass("comment-email text-end fw-light")
+          .text(comment.email)
+          .appendTo(divComment);
+        $(divComment).appendTo(".accordion-body");
+      });
+    }
+  );
+}
+
 function postData(postId) {
   var settings = {
     url: "https://jsonplaceholder.typicode.com/posts/" + postId,
@@ -6,9 +42,10 @@ function postData(postId) {
   };
 
   $.ajax(settings).done(function (response) {
-    /* $("#modalTitle").text(""); */
     $(".modal-title").text(response.title);
     $(".modal-body p").text(response.body);
+    userData(response.userId);
+    commentsData(response.id);
   });
 }
 
