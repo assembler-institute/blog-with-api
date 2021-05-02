e9c3d61bbd7341bb72209555ac99a66d6d3ef6a1
orionalfa <orionalfacentauri@gmail.com>
2021-05-02 21:34:42 +0200

    changed to jquery

--- assets/dist/js/main.js
+++ assets/dist/js/main.js
@@ -1,24 +1,19 @@
 $.get("https://jsonplaceholder.typicode.com/posts", function (data) {
-  //console.log(JSON.stringify(data, null, 2));
-  //alert("Load was performed.");
-  console.log(data);
+  //console.log($("#postsWrapper"));
   for (post of data) {
-    appendNewPostCard("postsWrapper", "postCardTemplate");
-    displayAPIPostFields(
-      document.querySelector("#postsWrapper").lastElementChild,
-      post
-    );
+    appendNewPostCard();
+    displayAPIPostFields($("#postsWrapper").children().last(), post);
   }
 });
 
-function appendNewPostCard(containerId, templateId) {
-  const templateContent = document.querySelector(`#${templateId}`).content;
-  return document
-    .getElementById(containerId)
-    .appendChild(document.importNode(templateContent, true));
+function appendNewPostCard() {
+  $("#postCardTemplate")
+    .clone()
+    .removeClass("d-none")
+    .appendTo("#postsWrapper");
 }
 
 function displayAPIPostFields(postCard, postData) {
-  postCard.querySelector("p").innerText = postData.body;
-  postCard.querySelector("h3").innerText = postData.title;
+  postCard.find("p").html(postData.body);
+  postCard.find("h3").html(postData.title);
 }
