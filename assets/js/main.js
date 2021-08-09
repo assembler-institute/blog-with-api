3504ad334d4318a2006171e9dc45c5d734b2e392
Pere Serra Bosch <pere.serra.bosch@gmail.com>
2021-08-09 17:34:53 +0200

    added html structure

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var $toggleInput = document.getElementById("toggle__input");
+$toggleInput.addEventListener("click", switchStyles);
+
+/**
+ * Toggle styles when we press the switch button
+ */
+function switchStyles() {
+  const $html = document.querySelector("html");
+  const $toggle = document.getElementById("toggle");
+
+  $toggle.classList.toggle("enabled");
+  $html.classList.toggle("dark-mode");
+}
