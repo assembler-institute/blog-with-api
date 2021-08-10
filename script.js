3b2af2acf8bdddc0b24db7f88d7e5b1d080e6877
andarbech <andarbech1.com>
2021-08-10 10:45:19 +0200

    fech done

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 document.addEventListener('DOMContentLoaded', (event) => {
 
-    for(i=0; i<10; i++){
+    for (i = 0; i < 10; i++) {
 
-    var template = document.getElementById("card-temp").content;
-    let clone = document.importNode(template, true);
-    
-    document.getElementById("cards-container").appendChild(clone)
+        var template = document.getElementById("card-temp").content;
+        let clone = document.importNode(template, true);
+
+        document.getElementById("cards-container").appendChild(clone)
+
     }
-  });
+});
+fetch("https://jsonplaceholder.typicode.com/posts")
+    .then(response => response.json())
+
+.then(data => {
+    data.forEach(blog => {
+        let p = document.createElement("p")
+        p.textContent = blog.title
+        console.log(p);
+        let items = document.querySelector("#titleBlog")
+    });
+})
