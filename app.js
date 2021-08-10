61a02941e144d5747dbb96e3a2a73a1e867e7fde
paulMolano <paulmolanoo@gmail.com>
2021-08-10 12:47:55 +0200

    buttons

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 function adjHtml(a, b) {
-    let cardy = `<div class="card" id=${a}><p class="title">${b}</p><div><button class="edit" data-edit="${a}">Edit</button><button class="delete" data-delete="${b}">Delete</button></div></div>`;
-    return cardy;
+  let cardy = `<div class="card" id=${a}><p class="title">${b}</p><div><button class="edit" data-edit="${a}">Edit</button><button class="delete" data-delete="${b}">Delete</button></div></div>`;
+  return cardy;
 }
 
 function getPosts() {
-<<<<<<< HEAD
   return fetch("http://localhost:3000/posts")
     .then((response) => response.json())
     .then((data) => {
       let container = document.querySelector("#grid");
       data.forEach((element) => {
-        container.innerHTML += `<div class="card" id=${element.id}><p class="title" id="title" data-bs-toggle="modal" data-bs-target="#staticBackdrop">${element.title}</p><div><button class="edit" data-edit="${element.id}">Edit</button><button class="delete" data-delete="${element.id}">Delete</button></div></div>`;
+        container.innerHTML += `<div class="card" id=${element.id}><p class="title" id="title" data-bs-toggle="modal" data-bs-target="#staticBackdrop">${element.title}</p><div><button class="btn btn-warning" data-edit="${element.id}">Edit</button><button class="btn btn-danger" data-delete="${element.id}">Delete</button></div></div>`;
       });
       let titles = document.querySelectorAll(".title");
       titles.forEach((element) => {
@@ -19,15 +18,6 @@ function getPosts() {
           document.getElementById("staticBackdropLabel").innerHTML =
             textoTitulo;
           document.getElementById("modal-body").innerHTML = data.body;
-=======
-    return fetch("http://localhost:3000/posts")
-        .then((response) => response.json())
-        .then((data) => {
-            let container = document.querySelector("#grid");
-            data.forEach((element) => {
-                container.appendChild(adjHtml(element.id, element.title));
-            });
->>>>>>> c90e6d71474d3743d6dfd2cad047d69d7a8276e0
         });
       });
     });
@@ -36,7 +26,6 @@ function getPosts() {
 getPosts();
 
 function getComments() {
-<<<<<<< HEAD
   return fetch("http://localhost:3000/comments")
     .then((response) => response.json())
     .then((data) => {
@@ -55,23 +44,3 @@ function getUsers() {
       });
     });
 }
-=======
-    return fetch("http://localhost:3000/comments")
-        .then((response) => response.json())
-        .then((data) => {
-            data.forEach((element) => {
-                console.log(element.body);
-            });
-        });
-}
-
-function getUsers() {
-    return fetch("http://localhost:3000/users")
-        .then((response) => response.json())
-        .then((data) => {
-            data.forEach((element) => {
-                console.log(element.username);
-            });
-        });
-}
->>>>>>> c90e6d71474d3743d6dfd2cad047d69d7a8276e0
