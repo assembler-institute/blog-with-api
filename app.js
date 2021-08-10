7b3a12491a4d9cf31eeeb473ebde6a9c0eaf46f5
Yaiser <yaiserar@gmail.com>
2021-08-10 10:31:12 +0200

    titles and ids inserted

--- app.js
+++ app.js
@@ -1,59 +1,36 @@
-let cardy = `<template class="drawer" ><div class="card" id="card"><p class="title" id="title">I am the title</p></div></template>`;
+let cardy = `<div class="card" id="card"><p class="title" id="title">I am the title</p></div>`;
 
 let btnTest = document.querySelector(".button");
 
-btnTest.addEventListener("click", createCard);
+btnTest.addEventListener("click", getPosts);
 
 function getPosts() {
-  return fetch("http://localhost:3000/posts")
-    .then((response) => response.json())
-    .then((data) => {
-      let hola = data;
-      console.log(hola);
-      //   data.forEach((element) => {
-      //     let postTitle = element.title;
-      //     let elementID = element.id;
-      //     console.log(postTitle, elementID);
-      //   });
-    });
+    return fetch("http://localhost:3000/posts")
+        .then((response) => response.json())
+        .then((data) => {
+            let container = document.querySelector("#grid");
+            data.forEach((element) => {
+                container.innerHTML += `<div class="card" id=${element.id}><p class="title" id="title">${element.title}</p></div>`;
+            });
+        });
 }
 
 function getComments() {
-  return fetch("http://localhost:3000/comments")
-    .then((response) => response.json())
-    .then((data) => {
-      data.forEach((element) => {
-        //console.log(element.body);
-      });
-    });
+    return fetch("http://localhost:3000/comments")
+        .then((response) => response.json())
+        .then((data) => {
+            data.forEach((element) => {
+                //console.log(element.body);
+            });
+        });
 }
 
 function getUsers() {
-  return fetch("http://localhost:3000/users")
-    .then((response) => response.json())
-    .then((data) => {
-      data.forEach((element) => {
-        // console.log(element.username);
-      });
-    });
-}
-
-function createCard() {
-  let cardCont = document.getElementById("grid");
-  cardCont.insertAdjacentHTML("beforeend", cardy);
-  let rempCardy = document.querySelector(".drawer").content;
-  let copyCardy = document.importNode(rempCardy, true);
-  cardCont.appendChild(copyCardy);
-  //copyCardy.setAttribute(data - id, elementID);
-  drawCards();
-  //getComments();
-  //getUsers();
-}
-
-function drawCards() {
-  getPosts();
-  console.log(hola);
-  for (let index = 0; index < hola.length; index++) {
-    console.log("hola");
-  }
+    return fetch("http://localhost:3000/users")
+        .then((response) => response.json())
+        .then((data) => {
+            data.forEach((element) => {
+                // console.log(element.username);
+            });
+        });
 }
