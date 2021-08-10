887bc8af0b556cd4e3a82452505d59da02b9b66e
andarbech <andarbech1.com>
2021-08-10 16:58:00 +0200

    missing print modal

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
+var recievedData;
+
 document.addEventListener('DOMContentLoaded', (event) => {
-    
-    
+
+
     fetch("https://jsonplaceholder.typicode.com/posts")
-    .then(response => response.json())
+        .then(response => response.json())
 
     .then(data => {
-    data.forEach(blog => {
-        var template = document.getElementById("card-temp").content;
-        let clone = document.importNode(template, true);
-        
-        template.querySelector("figcaption").textContent = blog.title
-        
-        document.getElementById("cards-container").appendChild(clone)
+        let index = 0
+        data.forEach(blog => {
+            var template = document.getElementById("card-temp").content;
+            let clone = document.importNode(template, true);
+            document.getElementById("cards-container").appendChild(clone)
+            $figcaption = template.querySelector("figcaption")
+            $figcaption.textContent = blog.title
+            $figcaption.id = `${index}`
+            index++
+
         })
-})
+        recievedData = data;
+    })
 });
