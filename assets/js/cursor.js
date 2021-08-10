16b9fd71ec71c4903696413e73acc84fc166ff8c
Pere Serra Bosch <pere.serra.bosch@gmail.com>
2021-08-10 13:51:43 +0200

    post main

--- assets/js/cursor.js
+++ assets/js/cursor.js
@@ -1,4 +1,6 @@
-const cursor = document.querySelector('.cursor');
+const $bigBall = document.querySelector('.cursor__ball--big');
+const $smallBall = document.querySelector('.cursor__ball--small');
+const $hoverables = document.querySelectorAll('.hoverable');
 
 let mouseX = 0;
 let mouseY = 0;
@@ -6,24 +8,57 @@ let mouseY = 0;
 let cursorX = 0;
 let cursorY = 0;
 
-let speed = 0.5; // change to increase the ease
+let speed = 0.025; // change to increase the ease
+let fastSpeed = 1; // change to increase the ease
 
-function animate() {
+/**
+ * Hover an element
+ */
+onMouseHover = () => {
+  $bigBall.style.transform = 'scale(2)';
+}
+
+/**
+ * Hover out an element
+ */
+onMouseHoverOut = () => {
+  $bigBall.style.transform = 'scale(1)';
+}
+
+/**
+ * Animate
+ */
+animate = () => {
   let distX = mouseX - cursorX;
-  let distY = mouseY - cursorY;
+  let distY = mouseY - cursorY - 16;
 
   cursorX = cursorX + (distX * speed);
   cursorY = cursorY + (distY * speed);
 
-  cursor.style.left = cursorX + 'px';
-  cursor.style.top = cursorY + 'px';
+  $bigBall.style.left = cursorX + 'px';
+  $bigBall.style.top = cursorY + 'px';
+
+  cursorX = cursorX + (distX * fastSpeed);
+  cursorY = cursorY + (distY * fastSpeed);
+
+  $smallBall.style.left = cursorX + 10 + 'px';
+  $smallBall.style.top = cursorY + 7 + 'px';
 
   requestAnimationFrame(animate);
 }
 
 animate();
 
-document.addEventListener('mousemove', (event) => {
-  mouseX = event.pageX;
-  mouseY = event.pageY;
+// document.addEventListener('scroll', (e) => {
+//   requestAnimationFrame(animate);
+// })
+
+document.addEventListener('mousemove', (e) => {
+  mouseX = e.pageX;
+  mouseY = e.pageY;
 })
+
+for (let i = 0; i < $hoverables.length; i++) {
+  $hoverables[i].addEventListener('mouseenter', onMouseHover);
+  $hoverables[i].addEventListener('mouseleave', onMouseHoverOut);
+}
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 var $toggleInput = document.getElementById("toggle__input");
 $toggleInput.addEventListener("click", switchStyles);
@@ -12,4 +12,21 @@ function switchStyles() {
 
   $toggle.classList.toggle("enabled");
   $html.classList.toggle("dark-mode");
+}
+
+/**
+ * Fetch data
+ * 
+ * @param {String} section
+ * @param {Number} from
+ * @param {Number} limit
+ */
+async function fetchData(section = 'posts', from = 0, limit = 10) {
+  const baseUrl = "https://jsonplaceholder.typicode.com";
+
+  const data = await fetch(`${baseUrl}/${section}?_start=${from}&_limit=${limit}`)
+    .then((response) => response.json())
+  // .then((data) => console.log(data));
+
+  console.log(data)
 }
