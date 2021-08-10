fccbdbb9732d3b73353a5e5755dafde9903c39bb
tamtran <tamtran2885@gmail.com>
2021-08-10 10:37:21 +0200

    add modal functionality

--- assets/main.js
+++ assets/main.js
@@ -1,14 +1,50 @@
 //let postModal = document.getElementById('postModal')
-let listItems = document.querySelectorAll(".list-group-item")
+let listItems = document.querySelectorAll(".list-group-item");
+let btnClose = document.querySelector('.btn-close');
+let exampleModal = document.getElementById("exampleModal");
+
+btnClose.addEventListener("click", closeModal)
 listItems.forEach (element => {
     element.addEventListener("click", ()=> modalWindow(element))
 })
 
 function modalWindow(element){
-    element.focus()
+    // element.focus()
+    exampleModal.style.display = "block"
+    exampleModal.classList.add("show")
 }
 
+function closeModal() {
+  exampleModal.style.display = "none"
+}
 
-//postModal.addEventListener('shown.bs.modal', function () {
-  //myInput.focus()
-//})
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == exampleModal) {
+    closeModal()
+  }
+}
+
+// Variables of template
+const cards = document.querySelector(".cards");
+const template = document.getElementById("template-card").content;
+const fragment = document.createDocumentFragment();
+
+// Template 
+fetch("http://localhost:3000/posts")
+  .then((res) => res.json())
+  .then((data) => {
+    // console.log(data.length)
+    data.slice(0, 20).forEach((ele) => {
+      // console.log(ele)
+      template.querySelector(".card-img-top").setAttribute("src", "../assets/img/levitan.jpg")
+
+      template.querySelector(".card-title").textContent = ele.id + ". "  + ele.title
+      template.querySelector(".card-text").textContent = ele.body
+
+      let clone = document.importNode(template, true);
+      fragment.appendChild(clone);
+    }) 
+    cards.appendChild(fragment);
+  })
+  
