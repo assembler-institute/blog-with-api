7f8d5f461894e1ac8431dd19fb5ea09b777d73d5
Pere Serra Bosch <pere.serra.bosch@gmail.com>
2021-08-10 17:49:45 +0200

    added dynamic main post

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,17 +16,57 @@ function switchStyles() {
 
 /**
  * Fetch data
- * 
- * @param {String} section
+ *
  * @param {Number} from
  * @param {Number} limit
+ * @param {String} section
  */
-async function fetchData(section = 'posts', from = 0, limit = 10) {
+async function fetchData(section = "posts", from = 0, limit = 20) {
   const baseUrl = "https://jsonplaceholder.typicode.com";
 
-  const data = await fetch(`${baseUrl}/${section}?_start=${from}&_limit=${limit}`)
-    .then((response) => response.json())
-  // .then((data) => console.log(data));
+  let response = await fetch(`${baseUrl}/${section}?_start=${from}&_limit=${limit}`)
+    .then((response) => { return response.json() })
 
-  console.log(data)
+  return response;
 }
+
+/**
+ * Fill main post
+ */
+async function fillMainPost() {
+
+  const cards = await fetchData("posts", 0, 1);
+
+  const { userId, id, title } = { ...cards[0] }
+
+  const users = await fetchData("users", userId - 1, userId);
+
+  const { name } = { ...users[0] }
+
+  const templateCard = `
+    <template id="post-${id}">
+      <article class="post-main">
+        <span class="post-main__author">${name}</span>
+        <h1 class="post-main__title">${title}</h1>
+        <button
+          type="button"
+          class="btn btn-dark"
+          data-bs-toggle="modal"
+          data-bs-target="#postModal"
+        >
+          Read more
+        </button>
+      </article>
+    </template>
+  `;
+
+  const mainPost = document.getElementById("mainPost");
+  mainPost.insertAdjacentHTML("beforeend", templateCard);
+
+  const contentTemplate = document.getElementById(`post-${id}`).content;
+  const copyContent = document.importNode(contentTemplate, true);
+  mainPost.innerHTML = '';
+  mainPost.appendChild(copyContent);
+}
+
+fillMainPost();
