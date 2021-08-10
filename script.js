79e2e3027ea54ecc034d0585ef2c5c42c6235571
Josep Romero Ros <joseprrr@gmail.com>
2021-08-10 16:59:06 +0200

    creating branch

--- script.js
+++ script.js
@@ -6,12 +6,46 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
     .then(data => {
     data.forEach(blog => {
-        var template = document.getElementById("card-temp").content;
-        let clone = document.importNode(template, true);
-        
+        let template = document.getElementById("card-temp").content;
         template.querySelector("figcaption").textContent = blog.title
-        
+        template.querySelector('figure').setAttribute("data-id", blog.id)
+        template.querySelector('img').setAttribute("data-id", blog.id)
+        template.querySelector('figcaption').setAttribute("data-id", blog.id)
+        let clone = document.importNode(template, true);
         document.getElementById("cards-container").appendChild(clone)
-        })
+        
+    })
 })
 });
+
+// document.getElementById("edit-modal").addEventListener("show.bs.modal", function (event) {
+//     fetch("https://jsonplaceholder.typicode.com/posts")
+//     .then(response => response.json())
+//     .then(data => {
+//         data.forEach(post => {
+            
+//             if(post.id === event.target.id){
+
+                
+//             document.getElementById("post-title").setAttribute("value",)}
+            
+//         });
+        
+        
+//     })
+// })
+
+document.addEventListener("click", (event)=>{
+    if (event.target.matches('[data-bs-toggle="modal"]')){
+        fetch("https://jsonplaceholder.typicode.com/posts")
+        .then(response => response.json())
+        .then(data => {
+            data.forEach(post => {
+        
+                if(post.id === parseInt(event.target.dataset.id) ){
+                    document.getElementById("post-title").setAttribute("value", post.title)
+                }
+            })
+        })
+    }
+})
