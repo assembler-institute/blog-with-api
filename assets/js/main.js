f74dcc62295d26b302788b00126c1230493ae9f0
ParisArcos <84467828+ParisArcos@users.noreply.github.com>
2021-08-10 16:05:27 +0100

    getPostData(id)

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,8 +29,61 @@ function fetchData(section = "posts", from = 0, limit = 20) {
     .then((data) => console.log(data));
 }
 
+function getPostData(id) {
+  let postCard = {
+    card: [{}],
+  };
+  fetch(`assets/data/db.json`)
+    .then((response) => response.json())
+    .then((data) => {
+      let postDataId = id - 1;
+      postCard.card.post = data["posts"].find(
+        (element) => (element.id = postDataId)
+      );
+      postCard.card.user = data["users"].find(
+        (element) => (element.id = postCard.card.post.userId)
+      );
+      postCard.card.comments = [];
+      for (const comment of data["comments"]) {
+        if (comment.postId === postDataId) {
+          postCard.card.comments.push(comment);
+        }
+      }
+    });
+
+  return postCard;
+}
+
+function fillMainPost(id) {
+  getPostData(id);
+  console.log(postCard);
+  let infoCard = `<template id="post-${card["post"].id}">
+  <article class="post-main">
+  <span class="post-main__author">${card["user"].name}</span>
+  <h1 class="post-main__title">${card["post"].title}</h1>
+  <button
+    type="button"
+    class="btn btn-dark"
+    data-bs-toggle="modal"
+    data-bs-target="#postModal"
+  >
+    Dark
+  </button>
+</article>
+  </template>`;
+  let toInsertInfo = document.getElementById("mainPost");
+  toInsertInfo.insertAdjacentHTML("beforeend", infoCard);
+  let contentTemplate = document.getElementById(
+    `post-${card["post"].id}`
+  ).content;
+  let copyContent = document.importNode(contentTemplate, true);
+  toInsertInfo.lastChild.remove();
+  toInsertInfo.appendChild(copyContent);
+}
+
 function fetchLocalData(section) {
-  fetch(`assets/data/${section}.json`)
+  const baseUrl = "assets/data/";
+  fetch(`${baseUrl}/${section}.json`)
     .then((response) => response.json())
     .then((data) => console.log(data));
 }
