0404e537151075479d58b2abf297bce2dbae204c
Josep Romero Ros <joseprrr@gmail.com>
2021-08-10 11:25:03 +0200

    printing all cards and titles

--- script.js
+++ script.js
@@ -1,22 +1,17 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-
-    for (i = 0; i < 10; i++) {
-
-        var template = document.getElementById("card-temp").content;
-        let clone = document.importNode(template, true);
-
-        document.getElementById("cards-container").appendChild(clone)
-
-    }
-});
-fetch("https://jsonplaceholder.typicode.com/posts")
+    
+    
+    fetch("https://jsonplaceholder.typicode.com/posts")
     .then(response => response.json())
 
-.then(data => {
+    .then(data => {
     data.forEach(blog => {
-        let p = document.createElement("p")
-        p.textContent = blog.title
-        console.log(p);
-        let items = document.querySelector("#titleBlog")
-    });
+        var template = document.getElementById("card-temp").content;
+        let clone = document.importNode(template, true);
+        
+        template.querySelector("figcaption").textContent = blog.title
+        
+        document.getElementById("cards-container").appendChild(clone)
+        })
 })
+});
