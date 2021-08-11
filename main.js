6369134ce1abf08b48d6fb2a07f54b679e080aec
gonzalogarahuetes <gonzaloarahuetes@gmail.com>
2021-08-11 12:47:04 +0200

    Function Commented

--- main.js
+++ main.js
@@ -22,17 +22,25 @@ document.getElementById("carousel-3").src =
 //   divCont.appendChild(row2);
 // });
 
-//---------------------------------------------------ADDING IDS TO CARDS
+//---------------------------------------------------ADDING IDS TO CARDS AND MORE
 
 let cards = document.querySelectorAll(".card");
 
 function ids() {
   for (let index = 0; index < cards.length; index++) {
+    //We are adding ids to the cards and buttons
+
     cards[index].id = `id-${index}`;
     let btnModal = cards[index].querySelector("a");
     btnModal.id = `btn-${index}`;
+
+    //Now we set the data-bs-toggle and data-bs-target which Bootstrap requires
+
     btnModal.dataset.bsToggle = "modal";
     btnModal.dataset.bsTarget = "#modal--main";
+
+    //We get the information of each post and fill the cards with it
+
     fetch(`https://jsonplaceholder.typicode.com/posts/${index + 1}`)
       .then((response) => {
         return response.json();
@@ -42,6 +50,8 @@ function ids() {
         cards[index].querySelector(".card-text").textContent =
           response.body.substring(0, 30) + "...";
       });
+
+    //We get a new picture for each card
 
     cards[index].querySelector("img").src = `https://picsum.photos/id/${
       index + 10 + index
