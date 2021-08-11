0fa5baf42944106f7ee7a35967f383657236cb84
Jose1i1o <guadix88@gmail.com>
2021-08-12 01:21:55 +0200

    feat(fill modals): inject api data to modals + reviews. Reviewed by Jose Valenzuela

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,25 +17,26 @@ let posts;
 // Fetch the data from the API with await response.json() returns a promise resolved to a JSON object
 
 async function getAllPosts() {
-    const RESPONSE = await fetch(
+    const ALLPOSTS = await fetch(
         `https://jsonplaceholder.typicode.com/posts/?_start=${initialPosts}&_limit=${limit}`
     );
-    const DATA = await RESPONSE.json();
-    return DATA;
+    let data = await ALLPOSTS.json();
+    return data;
 }
-
 // Let´s inject data to the DOM
 
 async function showAllPosts() {
     const POSTS = await getAllPosts();
     POSTS.forEach((post) => {
-        let eachPost = $(`<div class="col cardElement">
+        let eachPost =
+            $(`<div class="col" data-bs-toggle="modal" data-bs-target="#exampleModal">
                 <div id="postsmodal" class="card h-100">
                     <img src="assets/img/mjackson.jpg" class="card-img-top" alt="..." />
-                    <div class="card-body bg-dark">
-                        <p data-id="${post.id}"  data-bs-toggle="modal" data-bs-target="#exampleModal" class="card-title text-white" id="${post.id}" data-body="${post.body}" data-userid="${post.userId}">${post.id}&#160;&#160;${post.title}</p>
-                        </div>
-                        </div>)`);
+                    <div class="card-body background-color: var(--bs-white)">
+                        <p class="title" data-id="${post.id}" style="color: var(--bs-gray-900);" data-userid="${post.userId}" id="${post.id}" data-body="${post.body}">${post.id}&#160;&#160;${post.title}</p>
+                    </div> 
+                </div>
+            </div)`);
         postsContainer.append(eachPost);
     });
 }
@@ -53,24 +54,69 @@ $("#loadmore").on("click", function() {
     showAllPosts();
 });
 
-$(".card").on("click", function() {
-    $("#postsmodal").css("display", "block");
-    $("#postsmodal").css("opacity", "1");
-});
-
 $(document).on("click", function(element) {
-    if (element.target.matches(".card-title")) {
+    if (element.target.matches(".title")) {
         let getElements = {
-            url: `https://jsonplaceholder.typicode.com/users/${element.target.dataset.userid}`,
+            url: `https://jsonplaceholder.typicode.com/users/${element.target.dataset.userid}`, // Get each content using the userid
             method: "GET",
         };
-        $.ajax(getElements).document(function(response) {
+        $.ajax(getElements).done(function(ALLPOSTS) {
             post = element.target.id;
+            $("#username").text(ALLPOSTS.username);
+            $("#emailuser").text(ALLPOSTS.email);
             $("#bodypost").text(element.target.dataset.body);
-            $("#username").text(response.username);
-            $("#emailuser").text(response.email);
             let titlePost = $(`[data-id="${element.target.id}"]`).text();
             $(".modal-title").text(titlePost);
         });
+        $("#btn__showcomments").on("click", function() {
+            $(this).addClass("d-none");
+
+            var getComments = {
+                url: `https://jsonplaceholder.typicode.com/posts/${element.target.id}/comments`,
+                method: "GET",
+            };
+
+            $.ajax(getComments).done(function(reviews) {
+                const displaycmmts = $(".modal-content");
+                reviews.forEach((review) => {
+                    let eachComment = $(`<div class="modal-body2">
+                                                        <strong class="fs-4">${review.name}</strong>
+                                                        <span class="mt-2">${review.body}</span>
+                                                        <span class="text-muted mt-3">${review.email}</span>
+                                                        </div>`);
+                    displaycmmts.append(eachComment);
+
+                    $("#exampleModal").on("hide.bs.modal", function() {
+                        $("#btn__showcomments").removeClass("d-none");
+                        $(".modal-body2").remove();
+                        $("#load-comments").off();
+                    });
+                });
+            });
+        });
     }
 });
+
+// $("#btn__showcomments").on("click", function() {
+//     $(this).addClass("d-none");
+
+//     var getComments = {
+//         url: `https://jsonplaceholder.typicode.com/posts/${element.target.id}/comments`,
+//         method: "GET",
+//     };
+
+//     $.ajax(getComments).done(function(reviews) {
+//         const displaycmmts = $(".modal-content");
+//         reviews.forEach((review) => {
+//             let eachComment = $(`<div class="modal-body2">
+//         <strong class="fs-4">${review.name}</strong>
+//         <span class="mt-2">${review.body}</span>
+//         <span class="text-muted mt-3">${review.email}</span>
+//         </div>`);
+//             displaycmmts.append(eachComment);
+
+// $("#exampleModal").on("hide.bs.modal", function() {
+//     $("#btn__showcomments").removeClass("d-none");
+//     $(".modal-body2").remove();
+//     $("#load-comments").off();
+// });
