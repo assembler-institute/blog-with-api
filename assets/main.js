a5245a5fd8458bba13348887a9618203c9996383
tamtran <tamtran2885@gmail.com>
2021-08-12 10:14:13 +0200

    fix comments and user info, add images

--- assets/main.js
+++ assets/main.js
@@ -12,19 +12,29 @@ let editModal = document.getElementById("edit-Modal");
 const alertSuccess = document.querySelector(".alert-success")
 const alertDanger = document.querySelector(".alert-danger")
 
+// Images 
+const images = ["levitan.jpg", "levitan1.jpg", "levitan2.jpg", "levitan3.jpg", "levitan4.jpg", "levitan5.jpg"];
+
 // Template
 fetch("http://localhost:3000/posts")
   .then((res) => res.json())
   .then((data) => {
     //console.log(data.length);
-    data.slice(0, 20).forEach((ele) => {
-      //console.log(ele);
-      // template.querySelector(".card-img-top").setAttribute("src", "../assets/img/levitan.jpg")
+    data.forEach((ele) => {
 
+      // Choose images randomly
+      let random = Math.floor(Math.random() * images.length);
+      template.querySelector(".card-img-top").setAttribute("src", `../assets/img/levitan${random+1}.jpg`)
+
+      // Set data attributes to button of info modal
       template.querySelector(".btn-info").setAttribute("data-id", `${ele.id}`)
+      template.querySelector(".btn-info").setAttribute("data-userid", `${ele.userId}`)
+
+      // Set data attributes to button of edit modal
       template.querySelector(".btn-edit").setAttribute("data-id", `${ele.id}`)
+
+      // Add content to card
       template.querySelector(".card-title").textContent = ele.id + ". " + ele.title;
-      // template.querySelector(".card-text").textContent = ele.body;
 
       let clone = document.importNode(template, true);
       fragment.appendChild(clone);    
@@ -36,33 +46,29 @@ fetch("http://localhost:3000/posts")
     const modalBody = document.querySelector(".post-text");
     const modalUser = document.querySelector(".modal-user");
     const modalEmail = document.querySelector(".modal-email");
-    const modalComments = document.querySelector(".card-body-comment");
-    const modalCommentName = document.querySelector(".card-body-name");
-    const modalCommentEmail = document.querySelector(".card-body-email");
+    const cardBody = document.querySelector(".card-body");
+
+    // const modalCommentName = document.querySelector(".card-body-name");
+    // const modalCommentEmail = document.querySelector(".card-body-email");
+    // const modalComments = document.querySelector(".card-body-comment");
 
     // Modal to display info
     listBtn.forEach((element) => {
       element.addEventListener("click", event => {
-        // console.log(element.dataset.id)
-        displayModal(element.dataset.id)
+        displayModal(element.dataset.id,element.dataset.userid)
       });
     });
 
-    function displayModal(id) {
+    function displayModal(id,userId) {
       exampleModal.style.display = "block";
       exampleModal.classList.add("show");
-      // console.log(id)
-      displayModalInfo(id)
+
+      displayModalInfo(id,userId)
+      displayModalComments(id)
     }
 
-    function displayModalInfo(id) {
-      fetch("http://localhost:3000/users")
-        .then((res) => res.json())
-        .then((data) => {
-          // console.log(data[id-1])
-          modalUser.textContent = data[id-1].name
-          modalEmail.textContent = data[id-1].email
-        })
+    function displayModalInfo(id,userId) {
+    
 
       fetch("http://localhost:3000/posts")
         .then((res) => res.json())
@@ -71,12 +77,31 @@ fetch("http://localhost:3000/posts")
           modalBody.textContent = data[id-1].body
         })
 
-      fetch("http://localhost:3000/comments")
+        
+      fetch("http://localhost:3000/users")
         .then((res) => res.json())
         .then((data) => {
-          modalCommentName.textContent = data[id-1].name
-          modalCommentEmail.textContent = data[id-1].email
-          modalComments.textContent = data[id-1].body
+          // console.log(data[id-1])
+          modalUser.textContent = data[userId-1].name
+          modalEmail.textContent = data[userId-1].email
+        })
+    }
+
+    function displayModalComments(id) {
+      fetch(`http://localhost:3000/comments/?postId=${id}`)
+        .then((res) => res.json())
+        .then((data) => {
+          // console.log(data)
+          var html = ``;
+          data.forEach((element) => {
+            // console.log(element)
+            html += `
+            <p class="card-body-name" style="font-weight:bold;">${element.name}</p>
+            <p class="card-body-email">${element.email}</p>
+            <p class="card-body-comment">${element.body}</p>
+            `;
+          })
+          cardBody.innerHTML = html;
         })
     }
 
@@ -152,22 +177,3 @@ window.onclick = function (event) {
   }
 };
 
-// editModalBtn.forEach((element) => {
-//   element.addEventListener("click", event => {
-//     //console.log(element.dataset.id)
-//     displayModalEdit(element.dataset.id)
-//   });
-// });
-
-// let editModal = document.getElementById("edit-Modal")
-// function displayModalEdit(id) {
-//   editModal.style.display = "block";
-//   editModal.classList.add("show");
-//       // console.log(id)
-//       //displayModalEdit(id)
-// };
-
-// let btnCloseEdit = document.querySelectorAll(".btn-close-edit");
-// btnCloseEdit.forEach((element) => {
-//   element.addEventListener("click", ()=> {editModal.style.display = "none"})
-// });
