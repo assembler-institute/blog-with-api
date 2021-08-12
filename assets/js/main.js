c725e79cb59bc5266b74fd865d109a8eadc4d0f5
ParisArcos <84467828+ParisArcos@users.noreply.github.com>
2021-08-12 13:30:58 +0100

    deleteComments() fixed, editComments() added, recountComments() added

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,6 +33,8 @@ async function fetchData(section = "posts", from = 0, limit = 10) {
 
 /**
  * Fill main post
+ * 
+* @param {String} mainPost begining post
  */
 async function fillMainPost(mainPost = 0) {
   let cards = await fetchData("posts", mainPost, 1);
@@ -81,6 +83,9 @@ async function fillMainPost(mainPost = 0) {
 
 /**
  * Fill post of line Section
+ * 
+ *  @param {Number} linePost begining post
+ *  @param {Number} lineLimit number of post to fetch 
  */
 async function fillLinesSection(linePost = 1, lineLimit = 6) {
   let cards = await fetchData("posts", linePost, lineLimit);
@@ -127,6 +132,9 @@ async function fillLinesSection(linePost = 1, lineLimit = 6) {
 
 /**
  * Fill post of tinder Section
+ * 
+ *  @param {Number} tinderPost begining post
+ *  @param {Number} tinderLimit number of post to fetch
  */
 async function fillTinderSection(tinderPost = 7, tinderLimit = 6) {
 
@@ -177,6 +185,9 @@ async function fillTinderSection(tinderPost = 7, tinderLimit = 6) {
 
 /**
  * Fill post of line Section
+ * 
+ *  @param {Number} inlinePost begining post
+ *  @param {Number} inlineLimit number of post to fetch
  */
 async function fillInlineSection(inlinePost = 13, inlineLimit = 10) {
 
@@ -224,8 +235,10 @@ async function fillInlineSection(inlinePost = 13, inlineLimit = 10) {
 
 /**
  * Fill the modal when read more is clicked
+ * 
+ *  @param {Number} modalId id of  modal
  */
-async function fillModal(modalId, section) {
+async function fillModal(modalId) {
 
   const post = await fetchData("posts", modalId - 1, 1);
 
@@ -292,24 +305,23 @@ async function fillModal(modalId, section) {
 
 /**
  * Fill modal´s comments
+ * 
+ *  @param {Number} modalId id of  modal
+ *  @param {String} companyName name of user company 
  */
 async function fillModalComments(modalId, companyName) {
 
   const comments = await fetchData("comments", 0, 500);
 
-  let counter = 0;
-
   const commentArea = document.getElementById("comments-list");
 
   commentArea.innerHTML = "";
 
-  for (const comment of comments) {
+  for (let index = comments.length; index > 0 ; index--) {
 
-    const { postId, id, name, body } = { ...comment };
+    const { postId, id, name, body, } = { ...comments[index] };
 
     if (postId === modalId) {
-
-      counter++;
 
       const templateModalComment = `
         <template id="comment-template-${id}">
@@ -317,13 +329,13 @@ async function fillModalComments(modalId, companyName) {
             <i class="bi bi-emoji-sunglasses pr-4"></i>
             <blockquote class="blockquote">
               <div class="blockquote__header">
-                <p class="blockquote__name d-inline">${name}</p>
+                <p id="blockquote__name-${id}" class="blockquote__name d-inline">${name}</p>
                 <span class="blockquote__dot"> · </span>
                 <cite title="Source Title" class="blockquote__company-name">${companyName}</cite>
               </div>
-              <p class="blockquote__body">${body}</p>
+              <p id="blockquote__body-${id}" class="blockquote__body">${body}</p>
             </blockquote>
-            <div class="comments__buttons d-flex flex-column">
+            <div class="comments__buttons d-flex flex-column" data-id="${id}">
               <button type="button" class="comments__delete" data-id="${id}" id="comment-delete-${id}">
                 <i class="bi bi-trash"></i>
               </button>
@@ -345,7 +357,7 @@ async function fillModalComments(modalId, companyName) {
     }
   }
 
-  document.getElementById("totalComments").innerHTML = `${counter} comments`;
+  recountComments()
 }
 
 /**
@@ -358,6 +370,11 @@ async function fillModalComments(modalId, companyName) {
   fillInlineSection()
 })()
 
+/**
+ * Delete comment of post
+ * 
+ * @param {Object} e event 
+ */
 async function deleteComment(e) {
   const id = e.target.parentElement.dataset.id;
   const baseUrl = "https://jsonplaceholder.typicode.com";
@@ -367,21 +384,77 @@ async function deleteComment(e) {
   if (response.status === 200) {
     console.log("El comentario se ha borrado");
     document.getElementById(`comment-${id}`).remove();
+    recountComments();
   }
 
   return response
 }
 
-async function editComment(e) {
+/**
+ * Edit comment of post
+ * 
+ * @param {Object} e event 
+ */
+ function editComment(e) {
   const id = e.target.parentElement.dataset.id;
+
+  let nameContainer=document.getElementById(`blockquote__name-${id}`);
+  let prevName=nameContainer.innerHTML;
+
+  nameContainer.innerHTML=`
+    <textarea name="textareaName" rows="3" cols="40" id="name-text-area-${id}">
+      ${prevName}
+    </textarea> 
+    `
+  let textContainer=document.getElementById(`blockquote__body-${id}`);
+  let prevText= textContainer.innerHTML;
+
+  textContainer.innerHTML=`
+    <textarea name="textareaBody" rows="5" cols="40" id="body-text-area-${id}">
+      ${prevText}
+    </textarea> 
+    <button type="button" class="comments__save" id="comments__save-${id}">
+      save
+    </button>
+  `
+  document.getElementById(`comments__save-${id}`).addEventListener("click", function () {
+    saveEditComment(id)
+  })
+}
+
+/**
+ * Saves comment after edit
+ * 
+ * @param {Number} id id of comment 
+ */
+async function saveEditComment(id){
   const baseUrl = "https://jsonplaceholder.typicode.com";
-  let prevText = document.getElementById(`comment-body-${id}`).innerHTML
+  let finalName= document.getElementById(`name-text-area-${id}`).value;
+  document.getElementById(`blockquote__name-${id}`).innerHTML=finalName;
 
-  let response = await fetch(`${baseUrl}/comments/${id}`, { method: 'PUT' });
+  let finalText= document.getElementById(`body-text-area-${id}`).value;
+  document.getElementById(`blockquote__body-${id}`).innerHTML=finalText;
 
-  if (response.status === 200) {
-    console.log("El comentario se ha editado");
-  }
+    let response = await fetch(`${baseUrl}/comments/${id}`, {
+      method: 'PATCH',
+      body: JSON.stringify({
+        name: finalName,
+        body: finalText,
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    });
+    if (response.status === 200) {
+      console.log("El comentario se ha editado");
+    }
+    return response
+}
 
-  return response
+/**
+ * Recount and modify total comments
+ */
+function recountComments(){
+  const counter =document.getElementById("comments-list").children.length
+  document.getElementById("totalComments").innerHTML = `${counter} comments`;
 }
