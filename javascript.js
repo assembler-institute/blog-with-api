03808f2f77f55b9e4303d9d36b4287857d6672e2
Erick Ayala <erickayalrias@gmail.com>
2021-12-01 14:53:47 +0100

    filter posts

--- javascript.js
+++ javascript.js
@@ -1,7 +1,31 @@
-// fetch("http://localhost:3000/posts")
-// .then((request)=>{
-// return request.json()
-// })
-// .then((info)=>{
-//     console.log(info)
-// })
+
+fetch("http://localhost:3000/posts")
+.then((request)=>{
+return request.json()
+})
+.then((info)=>{
+    info.forEach(element => {
+        const { title, body, id } = element
+        const titleP = document.createElement("p")
+        titleP.textContent= `${title}`
+        titleP.setAttribute("class", "title")
+        const bodyP= document.createElement("p")
+        bodyP.textContent=`${body}` 
+        bodyP.setAttribute("class", "body")
+        const divPost= document.createElement("div")
+        divPost.setAttribute("id", `${id}`)
+        // divPost.setAttribute("id", "contentPost")
+        divPost.appendChild(titleP)
+        divPost.appendChild(bodyP)
+        document.getElementById("contentBlog").appendChild(divPost)
+        divPost.addEventListener("click", (e)=>{
+            const infoPosts= info.filter(post=> {console.log(divPost.id); if(post.id == divPost.id) {
+                return post}})
+            console.log(infoPosts)
+        })
+    });
+
+    // addeventListener
+    //  
+    // console.log(info.map(probando=> {if(probando.userId === 1) return probando}))
+})
