5b4ce2dee28de57f2ef115614d005e898d0be7a1
Adrián Serrano <adrianno_3@hotmail.com>
2021-12-01 16:27:01 +0100

    sharing content

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,15 @@
+var myModal = document.getElementById('myModal')
+var myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', function () {
+  myInput.focus()
+})
+
+
+
+
+
+
 var requestOptions = {
     method: 'GET',
     redirect: 'follow'
