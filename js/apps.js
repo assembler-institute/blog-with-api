57d7dc5140e7b6ea0e57202e140a70336e57fcba
RoyerMateu <rogermateu2001@gmail.com>
2021-12-02 11:37:11 +0100

    edit & delete button

--- js/apps.js
+++ js/apps.js
@@ -32,10 +32,19 @@ fetch("https://jsonplaceholder.typicode.com/posts/1/comments", //id 1
 
 //Add Event Listeners
 function openModal() {
-    console.log(mainRow)
+    
 }
 var mainRow = document.getElementsByClassName("mainRow")
 for (let i = 0; i < mainRow.length; i++) {
     mainRow[i].addEventListener("click", openModal)
 }
-
+var edit = document.getElementsByClassName("edit")
+for (let i = 0; i < edit.length; i++) {
+    edit[i].addEventListener("click", openModal)
+    edit[i].style.cursor = "pointer"
+}
+var del = document.getElementsByClassName("delete")
+for (let i = 0; i < del.length; i++) {
+    del[i].addEventListener("click", openModal)
+    del[i].style.cursor = "pointer"
+}
