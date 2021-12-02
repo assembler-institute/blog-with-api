50fef2fd7bf6d06888dc3a5bc1f55341daca4915
Raul Lopez <raul.lopez@singularfactory.net>
2021-12-02 13:19:41 +0000

    WIP edit modal

--- js/Modal/modal.js
+++ js/Modal/modal.js
@@ -1,13 +1,13 @@
 class Modal{
 
-    constructor(){
+    constructor(title, body){
         $("<div></div>").addClass("modal fade show").attr("tabindex", "-1").attr("style", "display: block;").css("background-color", "rgba(0,0,0,0.3)")
             .append($("<div></div>").addClass("modal-dialog modal-dialog-centered mw-100")
                 .append($("<div></div>").addClass("modal-content")
                     .append($("<div></div>").addClass("modal-body d-flex flex-column")
-                        .append($("<h3></h3>").addClass("modal-title").text("Title"))
-                        .append($("<img></img>").addClass("img-close").attr("src", "./assets/img/close.png"))
-                        .append($("<span></span>").text("body"))
+                        .append($("<h3></h3>").addClass("modal-title").text(title)) //edit
+                        .append($("<img></img>").addClass("img-close rounded-circle").attr("src", "./assets/img/close.png"))
+                        .append($("<span></span>").text(body)) //edit
                         .append($("<h5></h5>").addClass("modal-title").text("User"))
                         .append($("<span></span>").text("alex123"))
                         .append($("<span></span>").text("alex@mail.com"))
@@ -21,6 +21,12 @@ class Modal{
         .appendTo("body");
 
         $("img").on("click", function(){ $(".modal").remove(); });
+
+        if(title === undefined || body === undefined){
+            $("<input></input>").addClass("modal-title").attr("size", 10).text(title).prependTo(".modal-body");
+            $("<textarea></textarea>").addClass("modal-title").attr("columns", 10).text(body).insertAfter(".img-close");
+        }
+
     }
 
 }
--- js/apps.js
+++ js/apps.js
@@ -33,15 +33,26 @@ fetch("https://jsonplaceholder.typicode.com/posts/1/comments", //id 1
 })
 
 //Add Event Listeners
-function openModal() {
-    new Modal();
+function openModal(e) {
+    e.stopPropagation();
+    if(e.target instanceof SVGElement){
+        // console.log(e.target);
+        new Modal();
+    } else {
+        new Modal(e.target.parentNode.children[1].textContent, e.target.parentNode.children[2].textContent);
+    }
 }
 
+
+/*const rows = $(".mainRow");
+for(let i = 0; i < rows){
+
+}*/
 var mainRow = document.getElementsByClassName("mainRow")
-for (let i = 0; i < mainRow.length; i++) {
+for (let i = 0; i < mainRow.length -1 ; i++) {
     mainRow[i].addEventListener("click", openModal)
 }
-var edit = document.getElementsByClassName("edit")
+/*var edit = document.getElementsByClassName("edit")
 for (let i = 0; i < edit.length; i++) {
     edit[i].addEventListener("click", openModal)
     edit[i].style.cursor = "pointer"
@@ -50,4 +61,4 @@ var del = document.getElementsByClassName("delete")
 for (let i = 0; i < del.length; i++) {
     del[i].addEventListener("click", openModal)
     del[i].style.cursor = "pointer"
-}
+}*/
