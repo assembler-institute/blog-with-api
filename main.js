cf9d734973859579514f892bc3a6fbe2153417da
Albert Garcia F <alberd_beto@hotmail.com>
2021-12-02 15:54:37 +0100

    try img in post

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 let urlPosts = 'https://jsonplaceholder.typicode.com/posts/';
 let urlUsers = 'https://jsonplaceholder.typicode.com/users';
+let imgRandom = 'https://random.imagecdn.app/500/150';
 
 /**things to do user name - divs in html bootstrap + post dataGET & get users */
 
@@ -55,7 +56,7 @@ function getPost(id) {
 for (let id = 1; id < 101; id++) {
     getPost(id);
     if (id > 0) {
-        var post = $('<div class="col-md-6" id="postContainer' + id + '"><div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative post-box" id="box' + id + '"> <div class="col p-4 d-flex flex-column position-static"><h3 class="mb-3 titlePost" id="titlePost' + id + '">Featured post</h3><p class="card-text mb-auto bodyPost" id="bodyPost' + id + '"></p> <div class="post-creator"><strong class="d-inline-block mb-2 text-warning" id="userName' + id + '"></strong></div><div class="button-flex"><button type="button" class="btn btn-primary modalBtn"data-bs-toggle="modal" data-bs-target="#exampleModal" data-btn-id="' + id + '">Read post</button></div></div></div></div>')
+        var post = $('<div class="col-md-6" id="postContainer' + id + '"><div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative post-box" id="box' + id + '"><div class="col p-4 d-flex flex-column position-static"><div id="imgPost' + id + '"></div><h3 class="mb-3 titlePost" id="titlePost' + id + '">Featured post</h3><p class="card-text mb-auto bodyPost" id="bodyPost' + id + '"></p> <div class="post-creator"><strong class="d-inline-block mb-2 text-warning" id="userName' + id + '"></strong></div><div class="button-flex"><button type="button" class="btn btn-primary modalBtn"data-bs-toggle="modal" data-bs-target="#exampleModal" data-btn-id="' + id + '">Read post</button></div></div></div></div>')
         $('#postDiv').append(post);
     };
     if(id==100){
@@ -73,10 +74,17 @@ function getPostContent(urlPosts){
                 let bodyPost=document.getElementById("bodyPost"+(index+1));
                 titlePost.innerHTML=value.title;
                 bodyPost.innerHTML=value.body;
+                let imgPost=document.getElementById("imgPost"+(index+1));
+                let imgelement=document.createElement("img");
+                imgelement.setAttribute("src",imgRandom);
+                imgPost.innerHTML=imgelement;
+                
             })
         }
+        
     });
 }
+
 /*click button, insert in modal title and body content*/
 $(document).ready(function(){
     $(".col-md-6 div .col").on("click","button",function(e){
@@ -91,14 +99,3 @@ $(document).ready(function(){
 
     });
 });
-
-
-
-
-
-
-
-
-
-
-
