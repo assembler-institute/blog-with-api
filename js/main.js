20b86e76e437561ed1806229e710fce80e3332cd
Kory <50864697+Kory-ifcd0210@users.noreply.github.com>
2021-12-02 12:56:46 +0000

    function global getPost y printTittle

--- js/main.js
+++ js/main.js
@@ -3,10 +3,29 @@ var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
 })
 
-fetch("../data/posts.json")
-.then(response=>response.json())
-.then(data=>{
-    data.forEach(element=>{
-        console.log(element.title); 
+let arrayPost=[];
+//Get Tittles
+//data[0].title
+window.onload= printTitle();
+
+
+async function printTitle(){
+  arrayPost = await getPosts();
+  console.log(arrayPost)
+    for(var i in arrayPost)
+    {
+      console.log(arrayPost[i].title);
+      $(".titlePost").eq(i).text(arrayPost[i].title)
+    }
+}
+
+async function  getPosts(){
+  await fetch('../data/posts.json')
+  .then(response => response.json())
+  .then(data => {
+    data.forEach(function (element){
+      arrayPost.push(element);
     })
-});
+  })
+  return arrayPost;
+}
