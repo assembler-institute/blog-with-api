2e4341b578e3f59b467783b3e1b09f6f523d04fc
RoyerMateu <rogermateu2001@gmail.com>
2021-12-02 10:30:05 +0100

    add jquery + edit cancel button + open modal

--- js/apps.js
+++ js/apps.js
@@ -7,7 +7,7 @@ fetch("https://jsonplaceholder.typicode.com/posts",
     return res.json();
 })
 .then(function (json) {
-    console.log(json)
+    // console.log(json)
 })
 fetch("https://jsonplaceholder.typicode.com/users",
 {
@@ -17,7 +17,7 @@ fetch("https://jsonplaceholder.typicode.com/users",
     return res.json();
 })
 .then(function (json) {
-    console.log(json)
+    // console.log(json)
 })
 fetch("https://jsonplaceholder.typicode.com/posts/1/comments", //id 1
 {
@@ -27,5 +27,15 @@ fetch("https://jsonplaceholder.typicode.com/posts/1/comments", //id 1
     return res.json();
 })
 .then(function (json) {
-    console.log(json)
+    // console.log(json)
 })
+
+//Add Event Listeners
+function openModal() {
+    console.log(mainRow)
+}
+var mainRow = document.getElementsByClassName("mainRow")
+for (let i = 0; i < mainRow.length; i++) {
+    mainRow[i].addEventListener("click", openModal)
+}
+
