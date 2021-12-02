f8b8a10e1427e62cd4e9c9da8641a31e2a17b0ca
vla1 <vladis149@gmail.com>
2021-12-02 13:34:43 +0100

    insert content into modal

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ function getUserid(userId, postId) {
         "data": {
             id: userId,
         },
-        
+        "timeout": 0,
     };
 
     $.ajax(userSettings)
@@ -40,7 +40,7 @@ function getPost(id) {
         "data": {
             id: id,
         },
-        
+        "timeout": 0,
     };
 
     $.ajax(settings)
@@ -55,7 +55,7 @@ function getPost(id) {
 for (let id = 1; id < 101; id++) {
     getPost(id);
     if (id > 0) {
-        var post = $('<div class="col-md-6" id="postContainer' + id + '"><div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative post-box" id="box' + id + '"> <div class="col p-4 d-flex flex-column position-static"><h3 class="mb-3 titlePost" id="titlePost' + id + '">Featured post</h3><p class="card-text mb-auto bodyPost" id="bodyPost' + id + '"></p> <div class="post-creator"><strong class="d-inline-block mb-2 text-warning" id="userName' + id + '"></strong></div><div class="button-flex"><button type="button" class="btn btn-primary modalButton" data-toggle="modal" data-target="#exampleModal" data-btn-id="' + id + '">Read post</button></div></div></div></div>')
+        var post = $('<div class="col-md-6" id="postContainer' + id + '"><div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative post-box" id="box' + id + '"> <div class="col p-4 d-flex flex-column position-static"><h3 class="mb-3 titlePost" id="titlePost' + id + '">Featured post</h3><p class="card-text mb-auto bodyPost" id="bodyPost' + id + '"></p> <div class="post-creator"><strong class="d-inline-block mb-2 text-warning" id="userName' + id + '"></strong></div><div class="button-flex"><button type="button" class="btn btn-primary modalBtn"data-bs-toggle="modal" data-bs-target="#exampleModal" data-btn-id="' + id + '">Read post</button></div></div></div></div>')
         $('#postDiv').append(post);
     };
     if(id==100){
@@ -69,19 +69,30 @@ function getPostContent(urlPosts){
         url: urlPosts,
         success:function(response){
             $.each(response,function(index,value){
-                var titlePost=document.getElementById("titlePost"+(index+1));
-                var bodyPost=document.getElementById("bodyPost"+(index+1));
+                let titlePost=document.getElementById("titlePost"+(index+1));
+                let bodyPost=document.getElementById("bodyPost"+(index+1));
                 titlePost.innerHTML=value.title;
                 bodyPost.innerHTML=value.body;
             })
         }
     });
 }
+/*click button, insert in modal title and body content*/
+$(document).ready(function(){
+    $(".col-md-6 div .col").on("click","button",function(e){
+        var positionDiv=e.currentTarget.getAttribute("data-btn-id");
+        var modalBody=document.querySelector(".modal-body");
+        var modalTitle=document.getElementById("exampleModalLabel");
+        var titlePost=document.getElementById("titlePost"+positionDiv);
+        var bodyPost=document.getElementById("bodyPost"+positionDiv);
 
-/*coger modalBtn */
+        modalTitle.innerHTML=titlePost.innerHTML;
+        modalBody.innerHTML=bodyPost.innerHTML;
 
-var modalbuttonid = document.getElementsByClassName(modalButton);
-/**coger post id  */
+    });
+});
+
+
 
 
 
