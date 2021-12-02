51127138137c6147b2c953eb06073535d5083d88
gcachonc <g.cachon.c@gmail.com>
2021-12-02 12:50:15 +0100

    implement all posts

--- assets/JS/modal.js
+++ assets/JS/modal.js
@@ -2,14 +2,22 @@
 var modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
-var btn = document.querySelector(".one");
+var btn = document.getElementsByClassName("bi-pencil-square");
+
 console.log(btn);
+setTimeout(() => {
+    for (const iterator of btn) {
+        console.log(iterator);
+        iterator.addEventListener("click", createModal);
+        
+    }
+}, 1000);
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
-btn.onclick = function () {
+function createModal () {
     modal.style.display = "block";
 }
 
@@ -27,23 +35,23 @@ window.onclick = function (event) {
 
 /* Modal form */
 
-// Example starter JavaScript for disabling form submissions if there are invalid fields
-(function () {
-    'use strict'
+// // Example starter JavaScript for disabling form submissions if there are invalid fields
+// (function () {
+//     'use strict'
 
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.querySelectorAll('.needs-validation')
+//     // Fetch all the forms we want to apply custom Bootstrap validation styles to
+//     var forms = document.querySelectorAll('.needs-validation')
 
-    // Loop over them and prevent submission
-    Array.prototype.slice.call(forms)
-        .forEach(function (form) {
-            form.addEventListener('submit', function (event) {
-                if (!form.checkValidity()) {
-                    event.preventDefault()
-                    event.stopPropagation()
-                }
+//     // Loop over them and prevent submission
+//     Array.prototype.slice.call(forms)
+//         .forEach(function (form) {
+//             form.addEventListener('submit', function (event) {
+//                 if (!form.checkValidity()) {
+//                     event.preventDefault()
+//                     event.stopPropagation()
+//                 }
 
-                form.classList.add('was-validated')
-            }, false)
-        })
-})()
+//                 form.classList.add('was-validated')
+//             }, false)
+//         })
+// })()
--- assets/JS/posts.js
+++ assets/JS/posts.js
@@ -8,9 +8,27 @@ var requestOptions = {
 
 fetch("https://jsonplaceholder.typicode.com/posts", requestOptions)
     .then(response => response.json())
-    .then(result => localStorage.setItem("posts", JSON.stringify(result)))
+    .then(result => { result.forEach(element => {
+        // console.log(element);
+        var container = document.querySelector(".col-md-8");
+        var button = document.createElement("button");
+        button.setAttribute("id", element.id);
+        button.classList.add("accordion");
+        button.innerHTML = `${element.title}        <i class="bi bi-pencil-square ${element.id}"></i><i class="bi bi-trash"></i>`;
+        container.appendChild(button);
+        var panel = document.createElement("div");
+        panel.classList.add("panel");
+        container.appendChild(panel);
+        var parraf = document.createElement("p");
+        parraf.innerHTML = element.body;
+        panel.appendChild(parraf);
+        var btnComment = document.createElement("button");
+        btnComment.innerHTML = "Leave a comment";
+        panel.appendChild(btnComment);
 
-var posts = JSON.parse(localStorage.getItem('posts'))
+    });})
+
+// var posts = JSON.parse(localStorage.getItem('posts'))
 
 /* Get Users */
 
@@ -25,3 +43,20 @@ fetch("https://jsonplaceholder.typicode.com/users", requestOptions)
     .catch(error => console.log('error', error));
 
 var users = JSON.parse(localStorage.getItem('users'))
+
+/* Open accordion */
+setTimeout(() => {
+    var acc = document.getElementsByClassName("accordion");
+        var i;
+        for (i = 0; i < acc.length; i++) {
+          acc[i].addEventListener("click", function () {
+            this.classList.toggle("active");
+            var panel = this.nextElementSibling;
+            if (panel.style.display === "block") {
+              panel.style.display = "none";
+            } else {
+              panel.style.display = "block";
+            }
+          });
+        }
+}, 1000);
