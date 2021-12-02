4800bc5c68109c63d9e556a6ca577e1d66c9684e
rogerpuig <rogerpuigdemasa@gmail.com>
2021-12-02 10:48:12 +0100

    modal echo

--- src/js/post.js
+++ src/js/post.js
@@ -1,4 +1,12 @@
 
+var locModal = document.getElementById('myModal');
+var btnShow= document.querySelectorAll("button")[1]
 
 
+//show the modal
+btnShow.addEventListener('click', (e) => {
+    locModal.style.display = "block";
+    locModal.style.paddingRight = "17px";
+    locModal.className="modal fade show"; 
+});
 
