9b13a710a9730e7f857436e96fc313550015d798
Erick Ayala <erickayalrias@gmail.com>
2021-12-05 21:56:48 +0100

    page 1 page 2 etc, dinamic but its on work all working fine

--- javascript.js
+++ javascript.js
@@ -2,7 +2,27 @@ var ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 
 var tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
 var teens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
 
-
+//! Variables donde pondre cada una de los if
+var first= document.createElement("div")
+first.setAttribute("id", "contentFull")
+var second= document.createElement("div")
+second.setAttribute("id", "contentFull")
+var third= document.createElement("div")
+third.setAttribute("id", "contentFull")
+var fourth= document.createElement("div")
+fourth.setAttribute("id", "contentFull")
+var five= document.createElement("div")
+five.setAttribute("id", "contentFull")
+var six= document.createElement("div")
+six.setAttribute("id", "contentFull")
+var seven= document.createElement("div")
+seven.setAttribute("id", "contentFull")
+var eight= document.createElement("div")
+eight.setAttribute("id", "contentFull")
+var nine= document.createElement("div")
+nine.setAttribute("id", "contentFull")
+var ten= document.createElement("div")
+ten.setAttribute("id", "contentFull")
 
 const information= {
     title:"",
@@ -11,12 +31,9 @@ const information= {
     body:"",
     name:"",
     email:"",
-    // commentName:"",
-    // commentBody:"",
-    // commentEmail:""
 }
 // var probando
-
+function pr(){
 fetch("http://localhost:3000/posts")
 .then((request)=>{
 return request.json()
@@ -25,24 +42,53 @@ return request.json()
     info.forEach(element => {
         const { title, body, id} = element
         const numtoletters= convert_tens_hundreds(id)
-        const creatButton=document.createElement("button")
-        creatButton.setAttribute("class", "prueba btn btn-primary btn-lg")
-        creatButton.setAttribute("id", id)
+        // const creatButton=document.createElement("button")
+        // creatButton.setAttribute("class", `btn btn-primary btn-lg`)
+        // creatButton.setAttribute("id", id)
         // creatButton.setAttribute("value", "click")
-        creatButton.innerHTML="Show Info"
+        // creatButton.innerHTML="Show Info"
         // creatButton.addEventListener("click", ()=>{
         //     console.log("kjasndkjasn")
         // })
         loadPage(title, numtoletters, body, id)
-        document.getElementById(numtoletters).appendChild(creatButton)
+        if(id <= 10){
+          first.innerHTML += contentFUll
+          // document.getElementById("contentFull").innerHTML += contentFUll
+        }else if(id > 10 && id <= 20){
+          second.innerHTML += contentFUll
+          // console.log(second)
+        } else if(id > 20 && id <=30){
+        third.innerHTML += contentFUll
+        }else if(id >30 && id <= 40){
+          fourth.innerHTML += contentFUll
+        }else if(id >40 && id <= 50){
+          five.innerHTML += contentFUll
+        }else if(id >50 && id <= 60){
+          six.innerHTML += contentFUll
+        }else if(id > 60 && id<= 70){
+          seven.innerHTML += contentFUll
+        }else if (id > 70 && id<= 80){
+          eight.innerHTML+= contentFUll
+        }else if(id >80 && id <= 90){
+          nine.innerHTML += contentFUll
+        }else if(id >90 && id <=100){
+          ten.innerHTML+= contentFUll
+        }
+        // document.getElementById(numtoletters).appendChild(creatButton)
         // p= document.querySelector(".prueba")
         // p.addEventListener("click", ()=>{
         //     console.log("kajsndkjns")
         // })
-        ostia= $(".prueba")
-        $(ostia).on("click", (e)=>{
-            console.log(e.target.id)
-            infoPosts= info.filter(post=> {if(post.id == e.target.id) {
+        // $(`#${id}`).on("click", ()=>{
+        //   console.log("toco el id")
+        // })
+        // setInterval(() => {
+          
+        // }, 00);
+        setTimeout(() => {
+          $(`#${id}`).on("click", ()=>{
+            console.log($(`#${id}`)[0].id)
+            infoPosts= info.filter(post=> {if(post.id == $(`#${id}`)[0].id) {
                         return post}})
             // console.log($(ostia))
                     information.title= infoPosts[0].title
@@ -56,105 +102,15 @@ return request.json()
                         llamada()
                     }, 400);
             console.log(infoPosts)
-            // console.log("toco este")
-        })
-        // $(`${id}`).on("click", ()=>{
-        //     console.log("janskdjnas")
-        // })
-        // creatButton.addEventListener("click", ()=>{
-        //     console.log("kajsndkjns")
-        // })
-        // document.getElementsByClassName("prueba").addEventListener("click", ()=>{
-        //     console.log("akjsndkjsndjkn")
-        // })
-    
-        // var ppp= document.getElementById(id)
-        // ppp.addEventListener("click", ()=>{
-        //     console.log("funcionaaaaostiaaputaa")
-            
-        // })
-        // creatButton.addEventListener("click", ()=>{
-        //     console.log("kjasnkjdnakjsdn")
-        // })
-        // creatButton.forEach(element => {
-            //     element.addEventListener("click", ()=>{
-                //         console.log("kjasndkjn")
-                //     })
-                // });
-                // creatButton.setAttribute("value", "click")
-                // creatButton.innerHTML= "pepe"
-        // poresto.appendChild(creatButton)
-        // console.log(document.getElementsByClassName("card-body"))
-        // poresto=document.getElementsByClassName("card-body")
-        // const probando= document.getElementById(id)
-        // probando.addEventListener("click", (e)=>{
-        //     console.log("kjasndkjan")
-        // })
-        // creatButton.addEventListener("click", (e)=>{ 
-        //     e.prebentDefault()
-        //     console.log("akjsndkjasn")
-        // })
-        // // console.log(document.getElementById(id))
-        // $({id}).on("click", ()=>{
-        //     console.log("jaksdn")
-        // })
-
-        // document.getElementById(p).appendChild(divPost)
-        // console.log(document.getElementById(id))
-        // document.querySelector(".card-body").appendChild(divPost)
-        // console.log(document.querySelector(".card-body"))
-
-        // document.querySelector(".prueba").addEventListener("click", ()=>{
-        //     console.log("probsabdhabsd")
-        // })
-        // $(".prueba").on("click", ()=>{
-        //     console.log($(".prueba"))
-        // })
-        // $({id}).on("click", ()=>{
-        //     console.log("akjsdna")
-        // })
-        // document.getElementById(id).addEventListener("click", ()=>{
-        //     console.log("estas clicando")
-        // })
-        // document.querySelectorAll("title").textContent="jaskdn"
-        // $(".title").text(title);
-        // const titleP = document.createElement("p")
-        // titleP.textContent= `${title}`
-        // $(".title").html(titleP)
-        // console.log(title)
-        // document.querySelector(".mb-0").textContent="jasdb"
-        // document.querySelectorAll("title")
-        // $("#contentFull").append(contentFUll);
-        // document.getElementById("contentFull").innerHTML += contentFUll
-        // document.getElementById
-        // titleP.setAttribute("class", "title")
-        // const bodyP= document.createElement("p")
-        // bodyP.textContent=`${body}` 
-        // bodyP.setAttribute("class", "body")
-        // const divPost= document.createElement("div")
-        // divPost.setAttribute("id", `${id}`)
-        // // divPost.setAttribute("class", "d-block p-2 bg-primary text-white")
-        // divPost.appendChild(titleP)
-        // divPost.appendChild(bodyP)
-        // document.getElementById("contentBlog").appendChild(divPost)
-        // divPost.addEventListener("click", ()=>{
-        //     infoPosts= info.filter(post=> {if(post.id == divPost.id) {
-        //         return post}})
-        //         // console.log(infoPosts)
-        //         information.title= infoPosts[0].title
-        //         information.id= infoPosts[0].id
-        //         information.userId= infoPosts[0].userId
-        //         information.body=infoPosts[0].body
-        //         fetchUsersInfo(information.userId)
-        //         comments(information.userId)
-        //         // $("#myModal").modal('show');
-        //         setTimeout(() => {
-        //             llamada()
-        //         }, 400);
-        // })
+          })
+          // console.log(document.querySelector(`.${id}`))
+          // $(`.${id}`).on("click", ()=>{
+          //   console.log("entro?")
+          // })
+        }, 00);
     });
 })
-
+}
 function llamada(){
     // beginmodal()
     $("#myModal").modal('show'); 
@@ -248,23 +204,22 @@ function comments (postId){
     }
 
 
-function loadPage(title,id, body){
+function loadPage(title,id, body, idbutton){
     contentFUll=`
     <div class="card">
       <div class="card-header">
         <h5 class="mb-0">
-          <button class="btn btn-link" data-toggle="collapse" data-target="#${id}" aria-expanded="true" aria-controls="collapseOne">
+          <button class="btn  btn-link " data-toggle="collapse" data-target="#${id}" aria-expanded="true" aria-controls="collapseOne">
           ${title}
-          <h5 class="title"></h5>
           </button>
         </h5>
       </div>
       <div id="${id}" class="collapse hide pli" aria-labelledby="headingOne" data-parent="#contentFull">
-        <div class="card-body">
-        ${body}
+        <div class="card-body lp">
+        <h5>${body}</h5>
+        <button type="button" class=" prueba btn btn-info" id="${idbutton}">More</button>
         </div>
       </div>`
-      document.getElementById("contentFull").innerHTML += contentFUll
     //   document.getElementById("${Fullid}").addEventListener("click", ()=>{
     //       console.log("aknmsd faksmdf")
     //   })
@@ -285,9 +240,55 @@ function convert_tens_hundreds(num) {
     }
   }
 
-  var tt= document.getElementById("contentFull").children
-  setTimeout(() => {
-      console.log(tt.length)
+  // var tt= document.getElementById("contentFull").children
+  // setTimeout(() => {
+  //     console.log(tt.length)
       
-  }, 1000);
+  // }, 1000);
 // console.log(document.getElementById("contentFull").children)
+
+
+function chargeInf(data){
+  pr()
+  document.getElementById("dataContent").appendChild(data)
+}
+
+function changeData(other){
+  // return false
+  other.innerHTML=""
+  $("#contentFull").remove()
+  chargeInf(other)
+}
+// document.getElementById("secondbtn").addEventListener("click", ()=>{
+//   second.innerHTML=""
+//   $("#contentFull").remove()
+//   chargeInf(second)
+// })
+// document.getElementById("thirdbtn").addEventListener("click", ()=>{
+//   third.innerHTML=""
+//   $("#contentFull").remove()
+//   chargeInf(third)
+// })
+// document.getElementById("onebtn").addEventListener("click", ()=>{
+//   first.innerHTML=""
+//   $("#contentFull").remove()
+//   chargeInf(first)
+// })
+function buttons(){
+  contentbtnfunc= `<button type="button" class="btn btn-outline-info" onclick="changeData(first)">1</button>
+  <button type="button" class="btn btn-outline-info" onclick="changeData(second)" >2</button>
+  <button type="button" class="btn btn-outline-info" onclick="changeData(third)">3</button>
+  <button type="button" class="btn btn-outline-info" onclick="changeData(fourth)">4</button>
+  <button type="button" class="btn btn-outline-info" onclick="changeData(five)">5</button>
+  <button type="button" class="btn btn-outline-info" onclick="changeData(six)">6</button>
+  <button type="button" class="btn btn-outline-info" onclick="changeData(seven)">7</button>
+  <button type="button" class="btn btn-outline-info" onclick="changeData(eight)">8</button>
+  <button type="button" class="btn btn-outline-info" onclick="changeData(nine)">9</button>
+  <button type="button" class="btn btn-outline-info" onclick="changeData(ten)">10</button>`
+  document.getElementById("centerbtn").innerHTML = contentbtnfunc
+}
+window.onload = function(){
+  // All code comes here 
+  chargeInf(first)
+  buttons()
+}
