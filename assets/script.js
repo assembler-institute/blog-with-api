d8df1c4f29a2871b6c9f4fe522bfb9e5f207204f
Carla <91063081+cicu90@users.noreply.github.com>
2021-12-07 14:57:22 +0000

    function delete

--- assets/script.js
+++ assets/script.js
@@ -1,121 +1,155 @@
+var btnDeletePost = undefined;
+arrayIdsDelete=[];
+
+
 window.onload = init();
 var start;
-var totalPost=0;
-function init(){
-    start= 0;
+var totalPost = 0;
+
+function init() {
+    start = 0;
     postsFetchFun(start);
 }
 const buttonNext = document.getElementById("carousel-control-next-a")
 const buttonPrev = document.getElementById("carousel-control-prev-a")
 buttonNext.addEventListener("click", nextPost)
 buttonPrev.addEventListener("click", prevPost)
-function nextPost(){
+
+function nextPost() {
     start += 6
     postsFetchFun(start)
 }
-function prevPost(){
+
+function prevPost() {
     start -= 6
     postsFetchFun(start)
 }
-function postsFetchFun(start){
+
+function postsFetchFun(start) {
     var requestOptions = {
         method: 'GET',
         redirect: 'follow'
     };
     var limit = 6;
-    if(start < 0){
-        start = totalPost -6
+    if (start < 0) {
+        start = totalPost - 6
     }
-    if (start > totalPost){
-        start= 0
+    if (start > totalPost) {
+        start = 0
     }
     var urlPosts = "http://localhost:3000/posts?_start=" + start + "&_limit=" + limit + ""
     fetch(urlPosts, requestOptions)
-        .then(response =>{
+        .then(response => {
             totalPost = response.headers.get('X-Total-Count')
             return response.text()
         })
-        .then(result => {let data = JSON.parse(result)
-            console.log(data)
+        .then(result => {
+            let data = JSON.parse(result)
             updatePostsList(data)
         })
         .catch(error => console.log('error', error));
 }
-function updatePostsList(data){
+var img = document.createElement("img");
+img.src = "http://www.google.com/intl/en_com/images/logo_plain.png";
+
+var src = document.getElementById("header");
+// src.appendChild(img);
+
+function updatePostsList(data) {
     let parent = document.getElementById("container-card-a")
     while (parent.firstChild) {
         parent.removeChild(parent.firstChild)
     }
     data.forEach(post => {
+        if (arrayIdsDelete.includes(post.id+"")){
+            return;
+        }
         let div1 = document.createElement("div")
-        div1.innerHTML =  `<div class="card h-100">
+        div1.innerHTML = `<div class="card h-100" id="card${post.id}">
                             <div class="card-body">
-                            <h5 class="card-title">` + post.title + `</h5>
+                            <div id="showDescriptionModal${post.id}">
+                            <h5 class="card-title" >` + post.title + `</h5>
                             <p class="card-text">` + post.body + `</p>
+                            </div>
+                            <button id="editPost${post.id}" class="btn btn-success icon-pencil" data-edit="${post.id}"></button>
+                            <button id="btnDeletePost${post.id}" class="btn btn-danger icon-bin" data-delete="${post.id}"></button>
                             </div>
                             </div>`
         div1.setAttribute("class", "col")
-        div1.setAttribute("id","User-container-"+post.id)
-        div1.setAttribute("data-bs-toggle","modal")
-        div1.setAttribute("data-bs-target","#staticBackdrop")
+        div1.setAttribute("id", "User-container-" + post.id)
+        div1.setAttribute("data-bs-toggle", "modal")
+        div1.setAttribute("data-bs-target", "#staticBackdrop")
         parent.appendChild(div1)
+
+
+        btnDeletePost = document.getElementById("btnDeletePost" + post.id);
+        btnDeletePost.addEventListener("click", function (e) {
+            console.log(12)
+            deletePost(this.id);
+        })
+
         //div1.addEventListener("click",function (){
-            //console.log(post)
-            //findUserFetchFun(post)
-            //findCommentsFetchFun(post)
+        //console.log(post)
+        //findUserFetchFun(post)
+        //findCommentsFetchFun(post)
         //})
-        div1.addEventListener("click",function (){
-            myModal= document.getElementById("staticBackdropLabel")
+        modalEvent= document.getElementById("showDescriptionModal"+post.id);
+        modalEvent.addEventListener("click", function () {
+            myModal = document.getElementById("staticBackdropLabel")
             console.log(myModal)
             findUserFetchFun(post)
             findCommentsFetchFun(post)
         })
     });
 }
-function findUserFetchFun(post){
+
+function findUserFetchFun(post) {
     var requestOptions = {
         method: 'GET',
         redirect: 'follow'
     };
-    var urlFetch = "http://localhost:3000/users/"+post.userId
-    fetch( urlFetch, requestOptions)
-    .then(response => response.text())
-    .then(result => {let data = JSON.parse(result)
-        console.log(data)
-        //createModal(post, data)
-        modalContent(post, data)
-    })
-    .catch(error => console.log('error', error));
+    var urlFetch = "http://localhost:3000/users/" + post.userId
+    fetch(urlFetch, requestOptions)
+        .then(response => response.text())
+        .then(result => {
+            let data = JSON.parse(result)
+            console.log(data)
+            //createModal(post, data)
+            modalContent(post, data)
+        })
+        .catch(error => console.log('error', error));
 }
-function modalContent(post, data){
-    document.getElementById("staticBackdropLabel").textContent= post.title
-    document.getElementById("staticBackdropLabel").textContent +=" "+ data.name
-    document.getElementById("staticBackdropLabel").textContent +=" "+ data.email
-    document.getElementById("modal-content").textContent= post.body
+
+function modalContent(post, data) {
+    document.getElementById("staticBackdropLabel").textContent = post.title
+    document.getElementById("staticBackdropLabel").textContent += " " + data.name
+    document.getElementById("staticBackdropLabel").textContent += " " + data.email
+    document.getElementById("modal-content").textContent = post.body
 
 
     button1 = document.getElementById("comments-button")
-    console.log(button1)
-    button1.addEventListener("click", function (){
-        console.log(post)
-        findCommentsFetchFun (post)
+    button1.addEventListener("click", function () {
+        findCommentsFetchFun(post)
     })
 }
-function findCommentsFetchFun(post){
+
+function findCommentsFetchFun(post) {
     var requestOptions = {
         method: 'GET',
         redirect: 'follow'
     };
-    var urlFetch = "http://localhost:3000/comments?postId="+post.id
-    fetch( urlFetch, requestOptions)
-    .then(response => response.text())
-    .then(result => {let comments = JSON.parse(result)
-        console.log(comments)
-        createCommentsFun(comments)
-    })
-    .catch(error => console.log('error', error));
+    var urlFetch = "http://localhost:3000/comments?postId=" + post.id
+    fetch(urlFetch, requestOptions)
+        .then(response => response.text())
+        .then(result => {
+            let comments = JSON.parse(result)
+            console.log(comments)
+            createCommentsFun(comments)
+        })
+        .catch(error => console.log('error', error));
 }
-function createCommentsFun(comments){
+
+function createCommentsFun(comments) {
     // if (buttonDiv.child){
     //     buttonDiv.removeChild(child)
     // }
@@ -126,10 +160,33 @@ function createCommentsFun(comments){
     buttonDiv = document.querySelector(".modal-body")
     buttonDiv.appendChild(commentsContainer)
 
-    comments.forEach((comment)=>{
+    comments.forEach((comment) => {
 
         let commentDiv = document.createElement("div")
         commentDiv.innerHTML = comment.name
         commentsContainer.appendChild(commentDiv)
     })
 }
+
+
+function deletePost(id) {
+    var numericId=id.substring(13);
+    var card= document.getElementById("card"+numericId);
+    card.remove();
+
+    arrayIdsDelete.push(numericId);
+    console.log(arrayIdsDelete);
+
+    // btnDeletePost1
+    console.log("el id es "+id)
+    console.log("el id numerico es "+numericId)
+    // document.querySelector(post.title)
+    // fetch("http://localhost:3000/post/", {
+    //     method: "DELETE",
+
+    // })
+}
+
+// let arrayImg = [
+//     img= "assets/img/user-icon.png"
+// ]
