05b527f8ba661a45466cc1c323b6591da58da98d
MarcelDurganteDev <info@marceldurgante.dev>
2021-12-09 04:23:15 +0100

    copy JS from development

--- javascript.js
+++ javascript.js
@@ -1,28 +1,3 @@
-//! Variables, const
-var ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-var tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
-var teens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
-var first= document.createElement("div")
-first.setAttribute("id", "contentFull")
-var second= document.createElement("div")
-second.setAttribute("id", "contentFull")
-var third= document.createElement("div")
-third.setAttribute("id", "contentFull")
-var fourth= document.createElement("div")
-fourth.setAttribute("id", "contentFull")
-var five= document.createElement("div")
-five.setAttribute("id", "contentFull")
-var six= document.createElement("div")
-six.setAttribute("id", "contentFull")
-var seven= document.createElement("div")
-seven.setAttribute("id", "contentFull")
-var eight= document.createElement("div")
-eight.setAttribute("id", "contentFull")
-var nine= document.createElement("div")
-nine.setAttribute("id", "contentFull")
-var ten= document.createElement("div")
-ten.setAttribute("id", "contentFull")
-
 const information= {
     title:"",
     id:"",
@@ -30,82 +5,53 @@ const information= {
     body:"",
     name:"",
     email:"",
+    // commentName:"",
+    // commentBody:"",
+    // commentEmail:""
 }
-//! Functions
-// 
-function callDataFetch(){
+// var probando
+
 fetch("http://localhost:3000/posts")
 .then((request)=>{
 return request.json()
 })
 .then((info)=>{
     info.forEach(element => {
-        const { title, body, id} = element
-        const numtoletters= convert_tens_hundreds(id)
-        loadPage(title, numtoletters, body, id)
-        if(id <= 10){
-          first.innerHTML += contentFUll
-        }else if(id > 10 && id <= 20){
-          second.innerHTML += contentFUll
-        } else if(id > 20 && id <=30){
-        third.innerHTML += contentFUll
-        }else if(id >30 && id <= 40){
-          fourth.innerHTML += contentFUll
-        }else if(id >40 && id <= 50){
-          five.innerHTML += contentFUll
-        }else if(id >50 && id <= 60){
-          six.innerHTML += contentFUll
-        }else if(id > 60 && id<= 70){
-          seven.innerHTML += contentFUll
-        }else if (id > 70 && id<= 80){
-          eight.innerHTML+= contentFUll
-        }else if(id >80 && id <= 90){
-          nine.innerHTML += contentFUll
-        }else if(id >90 && id <=100){
-          ten.innerHTML+= contentFUll
-        }
+        const { title, body, id } = element
+        const titleP = document.createElement("p")
+        titleP.textContent= `${title}`
+        titleP.setAttribute("class", "title")
+        const bodyP= document.createElement("p")
+        bodyP.textContent=`${body}` 
+        bodyP.setAttribute("class", "body")
+        const divPost= document.createElement("div")
+        divPost.setAttribute("id", `${id}`)
+        // divPost.setAttribute("id", "contentPost")
+        divPost.appendChild(titleP)
+        divPost.appendChild(bodyP)
+        document.getElementById("contentBlog").appendChild(divPost)
+        divPost.addEventListener("click", ()=>{
+            infoPosts= info.filter(post=> {if(post.id == divPost.id) {
+                return post}})
+                // console.log(infoPosts)
+                information.title= infoPosts[0].title
+                information.id= infoPosts[0].id
+                information.userId= infoPosts[0].userId
+                information.body=infoPosts[0].body
+                fetchUsersInfo(information.userId)
+                comments(information.userId)
+                // $("#myModal").modal('show');
+                setTimeout(() => {
+                    llamada()
+                }, 400);
+        })
     });
 })
-}
-// to call again the btn
-function btncall(){
-  fetch("http://localhost:3000/posts")
-  .then((request)=>{
-  return request.json()
-  })
-  .then((info)=>{
-      info.forEach(element => {
-          const { title, body, id} = element
-          const numtoletters= convert_tens_hundreds(id)
-          loadPage(title, numtoletters, body, id)
-          setTimeout(() => {
-            $(`#${id}`).on("click", ()=>{
-              console.log($(`#${id}`)[0].id)
-              infoPosts= info.filter(post=> {if(post.id == $(`#${id}`)[0].id) {
-                          return post}})
-              // console.log($(ostia))
-                      information.title= infoPosts[0].title
-                      information.id= infoPosts[0].id
-                      information.userId= infoPosts[0].userId
-                      information.body=infoPosts[0].body
-                      fetchUsersInfo(information.userId)
-                      comments(information.userId)
-                      // $("#myModal").modal('show');
-                      setTimeout(() => {
-                          llamada()
-                      }, 400);
-              console.log(infoPosts)
-            })
-          }, 00);
-      });
-  })
-  }
 
-// Modal call
 function llamada(){
+    // beginmodal()
     $("#myModal").modal('show'); 
 }
-
 function beginmodal(){
     content=` <div id="myModal" class="modal fade" tabindex="-1">
     <div class="modal-dialog">
@@ -119,7 +65,7 @@ function beginmodal(){
                 <div id="userName"></div>
                 <div id="email"></div>
                 </div>
-                <div class="modal-body" id="comments"> <b>Comentarios</b>
+                <div class="modal-body" id="comments">
                 </div>
               <div class="modal-footer">
                   <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
@@ -132,9 +78,12 @@ function beginmodal(){
         </div>` 
     document.getElementById("modalview").innerHTML= content
     document.getElementById("title").innerHTML= information.title
-    document.getElementById("email").innerHTML= `<b>Email: </b> ${information.email}`
-    document.getElementById("userName").innerHTML= `<b>Name: </b>${information.name}`
+    document.getElementById("email").innerHTML= information.email
+    document.getElementById("userName").innerHTML= information.name
     document.getElementById("bodyFetch").innerHTML=information.body
+    // document.getElementById("comentClick").addEventListener("click", ()=>{
+       
+    // })
 }
 
 function fetchUsersInfo(idposts){
@@ -158,7 +107,7 @@ function comments (postId){
     return request.json()
     })
     .then((response)=>{
-      var postComment= response.filter((element)=>{
+       var postComment= response.filter((element)=>{
         //    console.log(element)
             if(element.postId == postId)
             return element
@@ -173,76 +122,20 @@ function comments (postId){
             document.getElementById("comments").appendChild(divComment)
             let divCommentEmail = document.createElement("div")
             divCommentEmail.setAttribute("class", "infoEmail")
-            divCommentEmail.innerHTML= `<b>Email:</b> ${email}`
+            divCommentEmail.innerHTML= email
             // console.log(divComment)
+            document.getElementById("comments").appendChild(divCommentEmail)
             let divCommentBody = document.createElement("div")
             divCommentBody.setAttribute("class", "infoBody")
             divCommentBody.innerHTML= body
             // console.log(divComment)
             document.getElementById("comments").appendChild(divCommentBody)
-            document.getElementById("comments").appendChild(divCommentEmail)
+
+
         })
+    //    information.commentName= postComment[0].name
+    //     information.commentBody= postComment[0].body
+    //     information.commentEmail= postComment[0].email 
+        // console.log(postComment)
     })
     }
-
-
-function loadPage(title,id, body, idbutton){
-    contentFUll=`
-    <div class="card">
-      <div class="card-header">
-        <h5 class="mb-0">
-          <button class="btn  btn-link " data-toggle="collapse" data-target="#${id}" aria-expanded="true" aria-controls="collapseOne">
-          ${title}
-          </button>
-        </h5>
-      </div>
-      <div id="${id}" class="collapse hide pli" aria-labelledby="headingOne" data-parent="#contentFull">
-        <div class="card-body lp">
-        <h5>${body}</h5>
-        <button type="button" class=" prueba btn btn-info" id="${idbutton}">More</button>
-        </div>
-      </div>`
-}
-
-function convert_tens_hundreds(num) {
-    if (num < 10) return ones[num];
-    else if (num >= 10 && num < 20) return teens[num - 10];
-    else if(num == 100){ return ones[Math.floor(num / 100)] + "hundred" + convert_tens_hundreds(num % 100);}
-    else {
-      return tens[Math.floor(num / 10)] + "" + ones[num % 10];
-    }
-  }
-
-
-function chargeInf(data){
-  callDataFetch()
-  document.getElementById("dataContent").appendChild(data)
-  btncall()
-}
-
-function changeData(other){
-  $("#contentFull").remove()
-  document.getElementById("dataContent").appendChild(other)
-  btncall()
-}
-function buttons(){
-  contentbtnfunc= `<button type="button" class="btn btn-outline-info" onclick="changeData(first)">1</button>
-  <button type="button" class="btn btn-outline-info" onclick="changeData(second)" >2</button>
-  <button type="button" class="btn btn-outline-info" onclick="changeData(third)">3</button>
-  <button type="button" class="btn btn-outline-info" onclick="changeData(fourth)">4</button>
-  <button type="button" class="btn btn-outline-info" onclick="changeData(five)">5</button>
-  <button type="button" class="btn btn-outline-info" onclick="changeData(six)">6</button>
-  <button type="button" class="btn btn-outline-info" onclick="changeData(seven)">7</button>
-  <button type="button" class="btn btn-outline-info" onclick="changeData(eight)">8</button>
-  <button type="button" class="btn btn-outline-info" onclick="changeData(nine)">9</button>
-  <button type="button" class="btn btn-outline-info" onclick="changeData(ten)">10</button>`
-  document.getElementById("centerbtn").innerHTML = contentbtnfunc
-}
-
-
-
-window.onload = function(){
-  // All code comes here 
-  chargeInf(first)
-  buttons()
-}
