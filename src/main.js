5867709d156de7b64d9c88995f93381820531a6b
oualidromli <oualid.romli@gmail.com>
2022-05-12 17:56:37 +0200

    Api called with flex column

--- src/main.js
+++ src/main.js
@@ -1,3 +1,25 @@
-const postTitle = document.getElementById('postTitle');
-const postBody = document.getElementById('postBody');
+const container = document.querySelector('.article__section--posts');
 
+const postUrl = 'http://localhost:3000/posts';
+
+fetch(postUrl)
+  .then((response) => response.json())
+  .then((data) => {
+    data.forEach((element) => {
+      let div = document.createElement('div');
+
+      //   Post title
+      let title = document.createElement('h3');
+      title.classList.add('section__post--title');
+      title.textContent = element.title;
+      div.appendChild(title);
+      //   post body
+      let body = document.createElement('p');
+      body.textContent = element.body;
+      div.appendChild(body);
+      console.log(body);
+
+      container.classList.add('flex-column');
+      container.appendChild(div);
+    });
+  });
