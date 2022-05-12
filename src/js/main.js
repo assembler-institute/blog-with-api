0dd52a71471929d3c971f95193557a356545748f
Victor Garcia-Vao <[victorgarciavao@gmail.com]>
2022-05-12 16:44:53 +0200

    added id attributes to titles

--- src/js/main.js
+++ src/js/main.js
@@ -1,28 +1,41 @@
-let url= 'http://localhost:3000/';
+let url = "http://localhost:3000/";
 
 const commentsData = fetch(`${url}comments`);
 const postsData = fetch(`${url}posts`);
 const usersData = fetch(`${url}users`);
 
 window.onload = () => {
-postsData
-.then(response => response.json())
-.then(data => {
-    showTitle(data)  
-});
-}
+  postsData
+    .then((response) => response.json())
+    .then((data) => {
+      printPostTitle(data);
+    });
+};
 
-
-function showTitle(postTitle) {
-  //   let postTitleContainer = document.getElementById("postTitleContainer");
-  console.log(postTitleContainer);
-
+function printPostTitle(postTitle) {
+  //Get the container from html
+  const postsTitlesContainer = document.getElementById("postsTitlesContainer");
+  //Create a list for the titles & append it to the container
+  const listContainer = document.createElement("ul");
+  listContainer.className = "blog__postTitle--listContainer";
+  postsTitlesContainer.append(listContainer);
+  //Iterate each post in posts.json
   postTitle.map((post) => {
-    let titleContainer = document.createElement("div");
+    let titleContainer = document.createElement("li");
     titleContainer.className = "post__title";
     titleContainer.textContent = post.title;
-    postTitleContainer.append(titleContainer);
-    // console.log(post);
+    titleContainer.id = post.id;
+    titleContainer.dataset.user = post.userId;
+    listContainer.append(titleContainer);
   });
-  //Take userId & postId
+ getUserId();
 }
+function getUserId(){
+    let title = document.querySelectorAll("[data-user]");
+    console.log(title);
+    title.addEventListener("click", (e) => {
+        console.log(e.target);
+
+    });
+}
+
