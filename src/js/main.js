cd735991628c35538d7a51551a0b3395d30b5c89
jose-cuevas <jgutifer@gmail.com>
2022-05-12 17:17:54 +0200

    main conteiner javascript ended_script linked to html correction

--- src/js/main.js
+++ src/js/main.js
@@ -1,27 +1,24 @@
 "use strict";
 const mainContainer = document.getElementById("mainContainer");
 
-
-
 const fetchPosts = fetch("http://localhost:3000/posts");
-    fetchPosts.then(function(response){
+fetchPosts
+  .then(function (response) {
     return response.json();
+  })
 
-    })
-
-    .then(function(data){
-        // console.log(data);
-        data.map(post => {
-            const title = post.title;
-            const body = post.body;
-            console.log(title);
-            console.log(body);
-            const postTitle = document.createElement('h2');
-            const postBody = document.createElement('p');
-            postTitle.textContent = title;
-            postBody.textContent = body;
-            mainContainer.append(postTitle);
-            mainContainer.append(postBody);
-        })
-
-    })
+  .then(function (data) {
+    // console.log(data);
+    data.map((post) => {
+      const title = post.title;
+      const body = post.body;
+      console.log(title);
+      console.log(body);
+      const postTitle = document.createElement("h2");
+      const postBody = document.createElement("p");
+      postTitle.textContent = title;
+      postBody.textContent = body;
+      mainContainer.append(postTitle);
+      mainContainer.append(postBody);
+    });
+  });
