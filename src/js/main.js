b789dd5240d76c25900892934b1c4f64024a7ab9
Alejandro Avila <alejandroaperez1994g@gmail.com>
2022-05-12 18:43:42 +0100

    add function to get post info in modal

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,10 @@
-import { fetchPosts } from "./api.js";
+import { fetchPosts, fetchUser } from "./api.js";
 import { createPost, addPaginators, updatePostsDisplay } from "./utils.js";
 
 const postList = document.getElementById("postList");
 const previousPage = document.getElementById("previous-page");
 const nextPage = document.getElementById("next-page");
+
 let startNumberPagination = 0;
 let endNumberPagination = 0;
 
@@ -24,6 +25,7 @@ const getPosts = (startNumber, endNumber) => {
         postList.append(liElement);
       }
     });
+    addMultiEvents();
   });
 };
 
@@ -41,9 +43,28 @@ nextPage.addEventListener("click", (e) => {
   getPosts(startNumberPagination + 10, endNumberPagination + 10);
 });
 
+function addMultiEvents() {
+  const modalTitle = document.getElementById("modalTitle");
+  const modalBody = document.getElementById("modal__post_body");
+  const userName = document.getElementById("user_name");
+  const userEmail = document.getElementById("user_email");
+
+  Array.from(postList.children).map((post) => {
+    const title = post.querySelector("h3");
+    title.addEventListener("click", (e) => {
+      let user = fetchUser(e.target.getAttribute("data-userId"));
+      user.then((data) => {
+        userName.textContent = data[0].username;
+        userEmail.textContent = data[0].email.toLowerCase();
+      });
+
+      modalTitle.textContent = e.target.textContent;
+      modalBody.textContent = e.target.nextSibling.textContent;
+    });
+  });
+}
+
 getPosts(0, 10);
 addPaginators();
-
-
 
 export { getPosts, postList };
