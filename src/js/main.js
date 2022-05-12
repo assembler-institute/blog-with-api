7fb5a782bb15ba0db138f1b6c18134994ea384c2
Victor Garcia-Vao <[victorgarciavao@gmail.com]>
2022-05-12 13:37:45 +0200

    added showTitle function

--- src/js/main.js
+++ src/js/main.js
@@ -4,8 +4,27 @@ const commentsData = fetch(`${url}comments`);
 const postsData = fetch(`${url}posts`);
 const usersData = fetch(`${url}users`);
 
+window.onload = () => {
 postsData
 .then(response => response.json())
 .then(data => {
-   
+    showTitle(data)  
 });
+}
+
+
+function showTitle(postTitle){
+// let postTitleContainer = document.getElementById(postTitleContainer);
+ console.log(postTitleContainer);
+ 
+
+    postTitle.map(post => {
+
+    let titleContainer = document.createElement("div")
+    titleContainer.className = "post__title";
+    titleContainer.textContent = post.title;
+    postTitleContainer.append(titleContainer);
+    console.log(post);
+    });
+    //Take userId & postId
+}
