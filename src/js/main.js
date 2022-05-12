e551c7f36954aa2e4be5ba5c945b25af71b32b58
Victor Garcia-Vao <[victorgarciavao@gmail.com]>
2022-05-12 18:40:43 +0200

    added bootstrap to main container and modal

--- src/js/main.js
+++ src/js/main.js
@@ -5,48 +5,55 @@ const postsData = fetch(`${url}posts`);
 const usersData = fetch(`${url}users`);
 
 window.onload = () => {
-  postsData
-    .then((response) => response.json())
-    .then((data) => {
-      printPostTitle(data);
-    });
+    postsData
+        .then((response) => response.json())
+        .then((data) => {
+            printPostTitle(data);
+        });
 };
 
 function printPostTitle(postTitle) {
-  //Get the container from html
-  const postsTitlesContainer = document.getElementById("postsTitlesContainer");
-  //Create a list for the titles & append it to the container
-  const listContainer = document.createElement("ul");
-  listContainer.className = "blog__postTitle--listContainer";
-  postsTitlesContainer.append(listContainer);
-  //Iterate each post in posts.json
-  postTitle.map((post) => {
-    let titleContainer = document.createElement("li");
-    titleContainer.className = "post__title";
-    titleContainer.textContent = post.title;
-    titleContainer.dataset.postId = post.id;
-    titleContainer.dataset.user = post.userId;
-    listContainer.append(titleContainer);
-  });
+    //Get the container from html
+    const postsTitlesContainer = document.getElementById("postsTitlesContainer");
+    //Create a list for the titles & append it to the container
+    const listContainer = document.createElement("ul");
+    listContainer.className = "list-group";
+    postsTitlesContainer.append(listContainer);
+    //Iterate each post in posts.json
+    postTitle.map((post) => {
+        let titleContainer = document.createElement("li");
+        titleContainer.className = "list-group-item";
+        titleContainer.textContent = post.title;
+        titleContainer.dataset.postId = post.id;
+        titleContainer.dataset.user = post.userId;
+        listContainer.append(titleContainer);
+    });
 
-  getUserId(postTitle, );
+    getUserId(postTitle);
 }
 
 function getUserId(postTitle) {
-  let title = document.querySelectorAll("[data-user]");
+    let title = document.querySelectorAll("[data-user]");
 
-  title.forEach((e) => {
-    e.addEventListener("click", () => {
-      let postId = e.dataset.postId;
-       //showModal(postTitle[postId].body)
-      console.log(postTitle[postId].body);
+    title.forEach((e) => {
+        e.setAttribute("data-bs-toggle", "modal");
+        e.setAttribute("data-bs-target", "#staticBackdrop");
+
+        e.addEventListener("click", () => {
+            let modalTitle = document.getElementById("staticBackdropLabel");
+            let modalBody = document.getElementById("bodyContent");
+            let postId = e.dataset.postId;
+
+            modalTitle.textContent = e.textContent;
+            modalBody.textContent = postTitle[postId - 1].body;
+
+        });
     });
-  });
 
 }
 
 function showModal(data) {
-  const firstModal = document.getElementById("postDialog");
-  firstModal.textContent = data;
-  firstModal.showModal();
+    const firstModal = document.getElementById("postDialog");
+    firstModal.textContent = data;
+    firstModal.showModal();
 }
