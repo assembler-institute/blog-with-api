e9d6b672d3b4cf1e5114371faa46ccbe291a6e57
adriavallesbaradad <adriavallesbaradad@gmail.com>
2022-05-12 17:14:13 +0200

    main container js ended

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,27 @@
 "use strict";
+const mainContainer = document.getElementById("mainContainer");
+
+
 
 const fetchPosts = fetch("http://localhost:3000/posts");
+    fetchPosts.then(function(response){
+    return response.json();
 
-console.log(fetchPosts);
+    })
+
+    .then(function(data){
+        // console.log(data);
+        data.map(post => {
+            const title = post.title;
+            const body = post.body;
+            console.log(title);
+            console.log(body);
+            const postTitle = document.createElement('h2');
+            const postBody = document.createElement('p');
+            postTitle.textContent = title;
+            postBody.textContent = body;
+            mainContainer.append(postTitle);
+            mainContainer.append(postBody);
+        })
+
+    })
