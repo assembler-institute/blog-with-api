2e7dffee5242085429cfbe22e09c28fe9b8e52ad
reduansj <reduansenoun@gmail.com>
2022-05-13 09:39:34 +0200

    feature(get all data json)

--- src/js/main.js
+++ src/js/main.js
@@ -12,4 +12,39 @@ window.onload = () => {
         .then((data) => {
             printPostTitle(data);
         });
+        getData();
 };
+
+  
+  function getData() {
+    let url = "http://localhost:3000/";
+    let commentsData = "hola";
+    let postsData ;
+    let usersData = "";
+    const fetchedComments = fetch(`${url}comments`);
+    const fetchedPosts = fetch(`${url}posts`);
+    const  fetchedUsers = fetch(`${url}users`);
+  
+    fetchedPosts
+      .then((response) => response.json())
+      .then((data) => {
+        postsData = data;
+      });
+    fetchedComments
+      .then((response) => response.json())
+      .then((data) => {
+        commentsData = data;
+      });
+      fetchedUsers
+      .then((response) => response.json())
+      .then((data) => {
+        usersData = data;
+        return usersData
+    });
+   console.log(usersData);
+     
+  }
+
+
+
+
