a332da648800e30bfe41a68b5164798b77194aa3
Alejandro Avila <alejandroaperez1994g@gmail.com>
2022-05-13 17:11:24 +0100

    change indent

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,18 @@
-import { fetchPosts, fetchUser, fetchComments, modifyPost, deletePost} from "./api.js";
-import { createPost, addPaginators, updatePostsDisplay, updateDisplay, startNumberPagination, endNumberPagination } from "./utils.js";
+import {
+  fetchPosts,
+  fetchUser,
+  fetchComments,
+  modifyPost,
+  deletePost,
+} from "./api.js";
+import {
+  createPost,
+  addPaginators,
+  updatePostsDisplay,
+  updateDisplay,
+  startNumberPagination,
+  endNumberPagination,
+} from "./utils.js";
 
 const postList = document.getElementById("postList");
 const btnComments = document.getElementById("btnComents");
@@ -11,7 +24,6 @@ const deleteBtn = document.getElementById("deleteButton");
 
 const getPosts = (startNumber, endNumber) => {
   fetchPosts().then((data) => {
-    // console.log(data);
     data.forEach((post, index) => {
       if (index >= startNumber && index < endNumber) {
         const liElement = createPost(
@@ -21,6 +33,7 @@ const getPosts = (startNumber, endNumber) => {
           post.body,
           index
         );
+
         postList.append(liElement);
       }
     });
@@ -28,21 +41,17 @@ const getPosts = (startNumber, endNumber) => {
   });
 };
 
-
-
 function addMultiEvents() {
   const modalTitle = document.getElementById("modalTitle");
   const modalBody = document.getElementById("modal__post_body");
   const userName = document.getElementById("user_name");
   const userEmail = document.getElementById("user_email");
 
-
-
   Array.from(postList.children).map((post) => {
     const title = post.querySelector("h3");
     const modifyBtn = post.querySelector("[data-modify]");
     const body = title.nextElementSibling;
-    modifyBtn.addEventListener("click", (e)=>{
+    modifyBtn.addEventListener("click", (e) => {
       let id = e.target.getAttribute("data-id");
       modifyTitle.value = title.textContent;
       modifyBody.value = body.textContent;
@@ -50,44 +59,45 @@ function addMultiEvents() {
     });
 
     const deleteButton = post.querySelector("[data-delete]");
-    deleteButton.addEventListener("click", (e)=>{
+    deleteButton.addEventListener("click", (e) => {
       const dataId = e.target.getAttribute("data-id");
       deleteBtn.setAttribute("data-id", dataId);
-      
-    })
+    });
 
     title.addEventListener("click", (e) => {
-      
-      if (btnComments.classList.value.includes("hide")){
+      if (btnComments.classList.value.includes("hide")) {
         btnComments.classList.remove("hide");
         commentsUl.classList.add("hide");
-        }
-      
+      }
+
       let user = fetchUser(e.target.getAttribute("data-userId"));
       user.then((data) => {
         userName.textContent = data[0].username;
         userEmail.textContent = data[0].email.toLowerCase();
       });
-      btnComments.setAttribute("data-postId",`${e.target.getAttribute("data-id")}`);
+      btnComments.setAttribute(
+        "data-postId",
+        `${e.target.getAttribute("data-id")}`
+      );
       modalTitle.textContent = e.target.textContent;
       modalBody.textContent = e.target.nextSibling.textContent;
     });
-
   });
-
 }
+
+const updatePosts = () => {
+  updatePostsDisplay();
+  getPosts(startNumberPagination, endNumberPagination);
+};
 
 saveBtn.addEventListener("click", (e) => {
   e.preventDefault();
 
   const title = document.getElementById("modifyTitle");
   const body = document.getElementById("modifyBody");
-  const id = title.getAttribute("data-id")
-
+  const id = title.getAttribute("data-id");
   modifyPost(id, title.value, body.value);
-  updatePostsDisplay();
-  getPosts(startNumberPagination, endNumberPagination)
-})
+});
 
 btnComments.addEventListener("click", (e) => {
   commentsUl.classList.toggle("hide");
@@ -96,38 +106,36 @@ btnComments.addEventListener("click", (e) => {
   fetchComments(postId).then((data) => {
     console.log(data);
     updateDisplay();
-    data.forEach(dataComment=> {
+    data.forEach((dataComment) => {
       const commentsLi = document.createElement("li");
       const commentsTitle = document.createElement("p");
       const commentsBody = document.createElement("p");
       const commentsUser = document.createElement("p");
-      commentsTitle.textContent=`Title: ${dataComment.name}`;
-      commentsBody.textContent=`Comment: ${dataComment.body}`;
-      commentsUser.textContent=`e.mail: ${dataComment.email}`;
+      commentsTitle.textContent = `Title: ${dataComment.name}`;
+      commentsBody.textContent = `Comment: ${dataComment.body}`;
+      commentsUser.textContent = `e.mail: ${dataComment.email}`;
       commentsLi.append(commentsTitle, commentsBody, commentsUser);
       commentsUl.appendChild(commentsLi);
-      commentsUl.classList.add("comments__ul")
-      commentsUl.setAttribute("data-bs-spy","scroll")
-      commentsUl.setAttribute("data-target",".navbar")
-      commentsUl.setAttribute("data-offset","'50'")
+      commentsUl.classList.add("comments__ul");
+      commentsUl.setAttribute("data-bs-spy", "scroll");
+      commentsUl.setAttribute("data-target", ".navbar");
+      commentsUl.setAttribute("data-offset", "'50'");
       commentsLi.classList.add("comments__li");
       commentsTitle.classList.add("comments__title");
-      commentsUser.classList.add("comments_user")
+      commentsUser.classList.add("comments_user");
       // commentsTitle,
-    })
-
+    });
   });
-})
-deleteBtn.addEventListener("click", (e) =>{
-  const dataId = e.target.getAttribute("data-id")
+});
+deleteBtn.addEventListener("click", (e) => {
+  const dataId = e.target.getAttribute("data-id");
   console.log(dataId);
   deletePost(dataId);
   updatePostsDisplay();
   getPosts(startNumberPagination, endNumberPagination);
-})
-
+});
 
 getPosts(0, 10);
 addPaginators();
 
-export { getPosts, postList, commentsUl};
+export { getPosts, postList, commentsUl, updatePosts };
