38661f0576d59bc56724f02fe5b8253d872da314
dariogmzfdz <dariogomezfdz@gmail.com>
2022-05-13 13:56:02 +0200

    feat(posts): added the posts with a dinamic id

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,37 @@
+const postContainer = document.querySelector("#postContainer");
+const postArray = ["", "", "", "", "", "", "", "", "", ""];
+
+// Get the API data
 fetch("http://localhost:3000/posts", {
         method: "GET",
     })
-    .then(function (res) {
-        return res.json();
+    .then(function (response) {
+        return response.json();
     })
-    .then(function (json) {
-        let postTitle = document.querySelectorAll("#postTitle")
+    .then(function (data) {
+        // Select the title/body form the html
+        /* let postTitle = document.querySelectorAll("#postTitle")
         let postBody = document.querySelectorAll("#postBody")
-        for (let i = 0; i < postTitle.length; i++) {
-            const titleApi = json[i];
+        for (let i = 0; i < 10; i++) {
+            const titleApi = data[i];
             postTitle[i].textContent = titleApi["title"]
             postBody[i].textContent = titleApi["body"]
-        }
+        } */
+        let index = 0;
+        postArray.map(function () {
+
+            let postTitle = document.createElement("div");
+            let postBody = document.createElement("div");
+
+            postTitle.setAttribute("id", index++);
+
+            const postData = data[postTitle.id];
+            console.log(postData);
+
+            postTitle.textContent = postData["title"];
+            postContainer.appendChild(postTitle);
+
+            postBody.textContent = postData["body"];
+            postContainer.appendChild(postBody);
+        });
     })
