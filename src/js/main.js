d111406c779e62ae0ed166b589e7519b3ab2827c
Ricardo Vieira <vieiraraba@gmail.com>
2022-05-13 11:36:11 +0200

    feat(posts): adding posts to the blog

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,32 @@
-let title = document.getElementById('title');
 let body = document.getElementById('body');
+const fetchData = fetch("http://localhost:3000/posts");
+const postsContainer = document.getElementById('postsContainer');
+const gridContainer = ["","","",""];
+let index = 0;
 
-fetch('./db.json')
-.then(function (response){
-    return response.json();
+fetchData
+.then(response => response.json())
+.then(data => {
+    const titleContainer = document.getElementById("data");
+    const bodyContainer = document.getElementById('body');
+    const title = data[0].title;
+    const body = data[0].body;
+    titleContainer.append(title);
+    bodyContainer.append(body);
+
 })
-.then(function (users){
-    console.log(users)
-    for (var i=0; i<data.length; i++){
-        document.getElementById('title').innerHtml +=
-        data[i].id+"=>"+data[i].title+"";
-        }
+
+gridContainer.map(function(row) {
+    let postsRow = document.createElement("div");
+    postsRow.setAttribute("id","postBox" + index++);
+    postsRow.classList.add("col-lg-6");
+
+    postsElement.map(function (){
+        let readMoreBtn = document.createElement("button");
+        readMoreBtn.classList.add("btn btn-primary");
+
+        postsRow.appendChild(readMoreBtn);
     })
-    .catch(function(error){
-        console.log(error)
-    });
+
+    postsContainer.appendChild(postsRow);
+})
