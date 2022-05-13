7ad1aa4e5b9f73d07f9a32c6007da6795119d7d0
MiguelD-PC <mikdominguez@gmail.com>
2022-05-13 20:45:18 +0200

    updates

--- src/js/utility.js
+++ src/js/utility.js
@@ -1,4 +1,4 @@
-function createPost(post) {
+function createPost(post, users) {
 
     let cardContainer = document.getElementById("card-container");
     let template = document.getElementById("cardTemplate");
@@ -12,9 +12,9 @@ function createPost(post) {
     // let modalOpenBtn = document.querySelector("[data-bs-target]")
     // modalOpenBtn.setAttribute('data-bs-target', `#staticBackdrop${post.id}`)
     // console.log(modalOpenBtn)
-    cardIMG.src = `https://picsum.photos/200/300?random=${post.id}`;
+    cardIMG.src = `https://picsum.photos/200/300?1random=${post.id}`;
     cardTitle.innerText = post.title;
-    cardText.innerText = post.body;
+    // cardText.innerText = post.body;
     cardComments.innerText = post.id;
     modalButton.setAttribute("data-bs-postID", post.id);
     template.hidden = false;
@@ -26,17 +26,19 @@ function handleModal(allPosts, postId, users) {
     const modalTemplate = document.getElementById('staticBackdrop')
     const cardModal = modalTemplate.cloneNode(true)
     const modalTitle = document.getElementById('staticBackdropLabel')
-    const modalUser = document.getElementById('modal-user')
+    const modalUserName = document.getElementById('modal-user-name')
+    const modalUserEmail = document.getElementById('modal-user-email')
     const modalBody = document.getElementById('modal-body');
     const modalUserImg = document.getElementById("userImg");
     cardModal.id = postId;
 
     allPosts.forEach(post => {
         if (post.title && post.id == postId) {
-            const userByPost = users.filter(user => user.id == post.userId)[0].name
+            const userByPost = users.filter(user => user.id == post.userId)[0]
             modalTitle.textContent = post.title
-            modalUser.textContent = `A post  by ${userByPost}`
-            modalUserImg.src =`https://randomuser.me/api/portraits/men/${post.userId}.jpg`;
+            modalUserName.textContent = `A post  by ${userByPost.name}`
+            modalUserEmail.textContent = userByPost.email
+            modalUserImg.src = `https://randomuser.me/api/portraits/men/${post.userId}.jpg`;
             modalBody.textContent = post.body;
         }
     });
