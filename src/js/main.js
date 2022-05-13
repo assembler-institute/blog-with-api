70887251984e73447607ea5f8ed61e1963c1649b
marcelbbarreiro <marcelbarreiro@gmail.com>
2022-05-13 14:26:51 +0200

    feat(DisplayPosts): Render all jason data inside cards Bootstrap

--- src/js/main.js
+++ src/js/main.js
@@ -9,60 +9,58 @@ const gridContainer = [
 
 let index = 0;
 
-fetchData
+fetchData // Get data from Json server
 .then(response => response.json())
 .then(data => {
-    const titleContainer = document.getElementById("data");
-    const bodyContainer = document.getElementById('body');
-    const title = data[1].title;
-    const body = data[1].body;
-
-    titleContainer.append(title);
-    bodyContainer.append(body);
-
-
-
-gridContainer.map(function(row) {
-    let postsRow = document.createElement("div");
-    postsRow.setAttribute("id","postBox");
-    postsRow.classList.add("col-lg-6");
-
-    row.map(function (content) {
-        let postContent = document.createElement("div");
-        let imgContainer = document.createElement("img");
-        postContent.setAttribute("id","postContent");
-        postContent.classList.add("card","mb-4","card-body");
-        imgContainer.src = imgTest;
-        imgContainer.classList.add("card-img-top");
-
-
-        content.map(function () {
-            let readMoreBtn = document.createElement("button");
-            let titlePost = document.createElement("div")
-            let bodyPost = document.createElement("div")
-
-            titlePost.setAttribute("id","titlePost");
-            titlePost.classList.add("card-title");
-            titlePost.textContent = title;
-
-            bodyPost.setAttribute("id","bodyPost");
-            bodyPost.classList.add("card-text");
-            bodyPost.textContent = body;
-
-            readMoreBtn.classList.add("btn","btn-primary");
-            readMoreBtn.textContent = "Read more →"
-
-            postContent.appendChild(titlePost);
-            postContent.appendChild(bodyPost);
-            postContent.appendChild(readMoreBtn);
-        })
-
-        postsRow.appendChild(imgContainer);
-        postsRow.appendChild(postContent);
-    });
-
-
-    postsContainer.appendChild(postsRow);
-})
+    displayPosts(gridContainer, data); 
 
 })
+
+function displayPosts(container,data) {
+    let index = 0;
+    container.map(function(row) {
+        let postsRow = document.createElement("div");
+        postsRow.setAttribute("id","postBox");
+        postsRow.classList.add("col-lg-6");
+    
+        row.map(function (content) {
+            let postContent = document.createElement("div");
+            let imgContainer = document.createElement("img");
+            postContent.setAttribute("id","postContent");
+            postContent.classList.add("card","mb-4","card-body");
+            imgContainer.src = imgTest;
+            imgContainer.classList.add("card-img-top");
+    
+    
+            content.map(function () {
+                let readMoreBtn = document.createElement("button");
+                let titlePost = document.createElement("div")
+                let bodyPost = document.createElement("div")
+    
+                titlePost.setAttribute("id","titlePost");
+                titlePost.classList.add("card-title");
+                titlePost.textContent = data[index].title;
+    
+                bodyPost.setAttribute("id","bodyPost");
+                bodyPost.classList.add("card-text");
+                bodyPost.textContent = data[index].body;
+    
+                readMoreBtn.classList.add("btn","btn-primary");
+                readMoreBtn.textContent = "Read more →"
+    
+                postContent.appendChild(titlePost);
+                postContent.appendChild(bodyPost);
+                postContent.appendChild(readMoreBtn);
+                index++;
+            })
+    
+            postsRow.appendChild(imgContainer);
+            postsRow.appendChild(postContent);
+        });
+    
+    
+        postsContainer.appendChild(postsRow);
+    })
+}
+
+
