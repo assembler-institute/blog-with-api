fa645a37baabe9a24d91ae3f6d57805e85085a19
Alejandro Gaerste <alejandrogaerste411@gmail.com>
2022-05-13 17:52:34 +0200

    feature: add showTitleBody & showUserEmail in the modal

--- src/js/info-modal.js
+++ src/js/info-modal.js
@@ -2,6 +2,38 @@
 function openPost() {
   const modalOpen = new bootstrap.Modal(document.getElementById("modal"));
   modalOpen.show();
-  console.log(modalOpen);
 }
-export { openPost };
+//show the content of body and title
+function showTitleBody(event) {
+  const postId = event.target.getAttribute("data-post-id");
+  const modalTitle = document.getElementById("modalTitle");
+  const modalBody = document.getElementById("modalText");
+  const postData = fetch("http://localhost:3000/posts");
+
+  postData
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      let obj = data.find((item) => item.id == postId);// whe could make typeof!!
+      modalTitle.textContent = obj.title;
+      modalBody.textContent = obj.body;
+    });
+}
+function showUserEmail(event) {
+  const userId = event.target.getAttribute("data-user-id");
+  const modalUser = document.getElementById("modalUsername");
+  const modalEmail = document.getElementById("modalEmail");
+  const userData = fetch("http://localhost:3000/users");
+  userData
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      let obj = data.find((item) => item.id == userId);// whe could make typeof!!
+      modalUser.textContent = obj.username;
+      modalEmail.textContent = obj.email;
+    });
+}
+//EXPORT
+export { openPost, showTitleBody, showUserEmail };
--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-import { openPost } from "./info-modal.js";
+import { openPost, showTitleBody, showUserEmail } from "./info-modal.js";
 
 // Fetch posts from api for posts, return as .json data, and pass to displayPosts function.
 function getPostData() {
@@ -24,14 +24,36 @@ function displayPosts(data) {
     const blogTitle = document.createElement("h3");
     const blogPost = document.createElement("p");
 
-    postContainer.classList.add("post__container", "shadow-sm", "mx-1", "col-3", "p-3", "mb-5", "bg-body", "rounded", "container-xxl");
+    postContainer.classList.add(
+      "post__container",
+      "shadow-sm",
+      "mx-1",
+      "col-3",
+      "p-3",
+      "mb-5",
+      "bg-body",
+      "rounded",
+      "container-xxl"
+    );
+    postContainer.setAttribute("data-post-id", `${post.id}`);
+    postContainer.setAttribute("data-user-id", `${post.userId}`);
+    
     blogTitle.classList.add("post__title");
+    blogTitle.setAttribute("data-post-id", `${post.id}`);
+    blogTitle.setAttribute("data-user-id", `${post.userId}`);
+    
     blogPost.classList.add("post__blog--post");
+    blogPost.setAttribute("data-post-id", `${post.id}`);
+    blogPost.setAttribute("data-user-id", `${post.userId}`);
     blogTitle.textContent = post.title;
     blogPost.textContent = post.body;
 
     postContainer.append(blogTitle, blogPost);
     dataContainer.append(postContainer);
-    postContainer.addEventListener("click", openPost);
+    postContainer.addEventListener("click", (e) => {
+      showTitleBody(e);
+      openPost();
+      showUserEmail(e);
+    });
   });
 }
