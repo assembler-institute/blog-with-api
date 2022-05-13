a73787dc52d6f015c820e87ac6bfa8d15d91fa48
Victor Garcia-Vao <[victorgarciavao@gmail.com]>
2022-05-13 13:41:20 +0200

    added comment functions

--- src/js/functions.js
+++ src/js/functions.js
@@ -1,4 +1,39 @@
-function printPostTitle(postTitle) {
+//Global variables
+let usersData;
+let postsComments;
+
+function getData() {
+    let url = "http://localhost:3000/";
+    const fetchedComments = fetch(`${url}comments`);
+    const fetchedPosts = fetch(`${url}posts`);
+    const fetchedUsers = fetch(`${url}users`);
+
+    fetchedPosts
+        .then((response) => response.json())
+        .then((data) => {
+            printPostTitle(data);
+        });
+    fetchedComments
+        .then((response) => response.json())
+        .then((data) => {
+            getComments(data);
+        });
+    fetchedUsers
+        .then((response) => response.json())
+        .then((data) => {
+            getUsers(data);
+        });
+}
+
+function getUsers(users) {
+    usersData = users;
+}
+
+function getComments(comments) {
+    postsComments = comments;
+}
+
+function printPostTitle(postData) {
     //Get the container from html
     const postsTitlesContainer = document.getElementById("postsTitlesContainer");
     //Create a list for the titles & append it to the container
@@ -6,37 +41,81 @@ function printPostTitle(postTitle) {
     listContainer.className = "list-group";
     postsTitlesContainer.append(listContainer);
     //Iterate each post in posts.json
-    postTitle.map((post) => {
+    postData.map((post) => {
         let titleContainer = document.createElement("li");
         titleContainer.className = "list-group-item";
         titleContainer.textContent = post.title;
-        titleContainer.dataset.postId = post.id - 1;
+        titleContainer.dataset.postId = post.id;
         titleContainer.dataset.user = post.userId;
         listContainer.append(titleContainer);
     });
 
-    getUserId(postTitle);
+    displayModal(postData);
 }
 
+function displayModal(postData) {
+    let userData = document.querySelectorAll("[data-user]");
+    userData.forEach((user) => {
+        user.setAttribute("data-bs-toggle", "modal");
+        user.setAttribute("data-bs-target", "#staticBackdrop");
 
-function getUserId(postTitle) {
-    let title = document.querySelectorAll("[data-user]");
-
-    title.forEach((e) => {
-        e.setAttribute("data-bs-toggle", "modal");
-        e.setAttribute("data-bs-target", "#staticBackdrop");
-
-        e.addEventListener("click", () => {
-            let modalTitle = document.getElementById("staticBackdropLabel");
-            let modalBody = document.getElementById("bodyContent");
-            let postId = e.dataset.postId;
-
-            modalTitle.textContent = e.textContent;
-            modalBody.textContent = postTitle[postId].body;
-
+        user.addEventListener("click", () => {
+            addTitle(user, postData);
+            addUsers(user);
+            addComments(user);
         });
     });
-
 }
 
-export {printPostTitle};
+function addTitle(user, postData) {
+    let modalTitle = document.getElementById("staticBackdropLabel");
+    let modalBody = document.getElementById("bodyContent");
+    let postId = user.dataset.postId;
+    modalTitle.textContent = user.textContent;
+    modalBody.textContent = postData[postId - 1].body;
+}
+
+function addUsers(user) {
+    let userId = user.dataset.user;
+    let userName = document.getElementById("userName");
+    let email = document.createElement("p");
+    userName.textContent = `${usersData[userId - 1].name}`;
+    email.textContent = usersData[userId - 1].email;
+    userName.append(email);
+}
+
+function addComments(user) {
+    let commentContainer = document.getElementById("commentContainer");
+    commentContainer.textContent="";
+    // let idComment = postsComments.
+    // console.log(postsComments);
+    // let postId= user.dataset.postId;
+    // let name = document.createElement("p");
+    // let coment = document.createElement("p");
+    // name.textContent= postsComments;
+
+    postsComments.map((comment) => {
+
+
+        if (comment.postId == user.dataset.postId) {
+            let commentWrapper = document.createElement("div");
+            commentWrapper.className = "commentWrapper";
+            
+
+            let commentName = document.createElement("p");
+            let commentBody = document.createElement("p");
+            let email = document.createElement("p");
+            commentName.textContent= comment.name;
+            commentBody.textContent= comment.body;
+            email.textContent= comment.email;
+            commentContainer.append(commentWrapper);
+            commentWrapper.append(commentName, commentBody, email);
+            console.log(commentName);
+        }
+
+    });
+}
+
+export {
+    getData
+};
--- src/js/main.js
+++ src/js/main.js
@@ -1,49 +1,14 @@
-import {printPostTitle} from "./functions.js";
+import {getData} from "./functions.js";
 
-let url = "http://localhost:3000/";
-
-const commentsData = fetch(`${url}comments`);
-const postsData = fetch(`${url}posts`);
-const usersData = fetch(`${url}users`);
 
 window.onload = () => {
-    postsData
-        .then((response) => response.json())
-        .then((data) => {
-            printPostTitle(data);
-        });
         getData();
 };
 
+
   
-  function getData() {
-    let url = "http://localhost:3000/";
-    let commentsData = "hola";
-    let postsData ;
-    let usersData = "";
-    const fetchedComments = fetch(`${url}comments`);
-    const fetchedPosts = fetch(`${url}posts`);
-    const  fetchedUsers = fetch(`${url}users`);
-  
-    fetchedPosts
-      .then((response) => response.json())
-      .then((data) => {
-        postsData = data;
-      });
-    fetchedComments
-      .then((response) => response.json())
-      .then((data) => {
-        commentsData = data;
-      });
-      fetchedUsers
-      .then((response) => response.json())
-      .then((data) => {
-        usersData = data;
-        return usersData
-    });
-   console.log(usersData);
-     
-  }
+
+
 
 
 
