328585c8783e694bbb38da0f004f3cac8b42a22d
dariogmzfdz <dariogomezfdz@gmail.com>
2022-05-13 11:02:42 +0200

    feat(fetch): added the fecth function

--- src/js/main.js
+++ src/js/main.js
@@ -1,58 +1,15 @@
-/* const fetchApi = fetch("http://localhost:3000/posts/1");
-
-console.log(fetchApi);
-
-fetchApi
-    .then(response => response.json())
-    .then(data => {
-        const title = data.title;
-
-        const titleElement = document.createElement("h2");
-
-        titleElement.textContent = title;
-
-        const printTitle = document.getElementById("title");
-
-        printTitle.append(titleElement);
-    }) */
-
-let from = 0;
-let limit = 10;
-let mainUrl = "http://localhost:3000/"; //https://jsonplaceholder.typicode.com/
-let section;
-let id;
-let userId;
-let postId;
-let title;
-let body;
-
-$(function () {
-    loadPosts();
-});
-
-function loadPosts() {
-    section = "posts/";
-    var allPosts = {
-        url: `${mainUrl}${section}/?_start=${from}&_limit=${limit}`,
+fetch("http://localhost:3000/posts", {
         method: "GET",
-        timeout: 0,
-    };
-
-    $.ajax(allPosts).done(function (response) {
-        $(response).each(function (i, e) {
-            createPostWithTitle(e.title, e.id, e.userId);
-        });
-        $(".post").each(function (i, e) {
-            $(this).on("click", function () {
-                cleanPostBodyUserEmail();
-                userId = $(this).attr("user-id");
-                postId = $(this).attr("post-id");
-                $("#modal-for-posts").attr("post-id", postId);
-                $("#modal-for-posts").attr("user-id", userId);
-                loadBodyOfPost(postId);
-                loadUserAndEmail(userId);
-                loadCommentsOfPost(postId);
-            });
-        });
-    });
-}
+    })
+    .then(function (res) {
+        return res.json();
+    })
+    .then(function (json) {
+        let postTitle = document.querySelectorAll("#postTitle")
+        let postBody = document.querySelectorAll("#postBody")
+        for (let i = 0; i < postTitle.length; i++) {
+            const titleApi = json[i];
+            postTitle[i].textContent = titleApi["title"]
+            postBody[i].textContent = titleApi["body"]
+        }
+    })
