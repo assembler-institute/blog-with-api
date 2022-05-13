ba3813b567be66bc35e0c5db67f0100466c7d28f
Ricardo Vieira <vieiraraba@gmail.com>
2022-05-13 12:35:37 +0200

    feat(postsContainer): adding the title and the body to the post

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,10 @@
-let body = document.getElementById('body');
 const fetchData = fetch("http://localhost:3000/posts");
 const postsContainer = document.getElementById('postsContainer');
-const gridContainer = ["","","",""];
+
+const gridContainer = [
+    [[""]],[[""]],
+    [[""]],[[""]]];
+
 let index = 0;
 
 fetchData
@@ -9,24 +12,41 @@ fetchData
 .then(data => {
     const titleContainer = document.getElementById("data");
     const bodyContainer = document.getElementById('body');
-    const title = data[0].title;
-    const body = data[0].body;
+    const title = data[1].title;
+    const body = data[1].body;
+
     titleContainer.append(title);
     bodyContainer.append(body);
 
-})
+
 
 gridContainer.map(function(row) {
     let postsRow = document.createElement("div");
-    postsRow.setAttribute("id","postBox" + index++);
-    postsRow.classList.add("col-lg-6");
+    postsRow.setAttribute("id","postBox");
+    postsRow.classList.add("col-lg-6","mb-4");
 
-    postsElement.map(function (){
+    row.map(function () {
         let readMoreBtn = document.createElement("button");
-        readMoreBtn.classList.add("btn btn-primary");
+        let titlePost = document.createElement("div")
+        let bodyPost = document.createElement("div")
 
+        titlePost.setAttribute("id","titlePost");
+        titlePost.classList.add("card-title");
+        titlePost.textContent = title;
+
+        bodyPost.setAttribute("id","bodyPost");
+        bodyPost.classList.add("card-text");
+        bodyPost.textContent = body;
+
+        readMoreBtn.classList.add("btn","btn-primary");
+        readMoreBtn.textContent = "read more"
+
+        postsRow.appendChild(titlePost);
+        postsRow.appendChild(bodyPost);
         postsRow.appendChild(readMoreBtn);
     })
 
     postsContainer.appendChild(postsRow);
+})
+
 })
