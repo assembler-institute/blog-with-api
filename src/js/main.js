690274ed3b3a0b52e96a6751823a041a9ad24654
jose-cuevas <jgutifer@gmail.com>
2022-05-13 09:33:23 +0200

    modal posts beggining

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,17 @@
 "use strict";
+
+// DOM elements
 const mainContainer = document.getElementById("mainContainer");
+const postMain = document.getElementById("postMain");
+
+const postModal = document.getElementById("postModal");
+
+// console.log(postMain);
+
+// -----------------
+// Global Arrays from fecth
+let postsArray = [];
+let usersArray = [];
 
 const fetchPosts = fetch("http://localhost:3000/posts");
 fetchPosts
@@ -10,15 +22,70 @@ fetchPosts
   .then(function (data) {
     // console.log(data);
     data.map((post) => {
+      //   console.log(post);
+      postsArray.push(post);
       const title = post.title;
       const body = post.body;
-      console.log(title);
-      console.log(body);
+      //   console.log(title);
+      //   console.log(body);
+
       const postTitle = document.createElement("h2");
       const postBody = document.createElement("p");
+
       postTitle.textContent = title;
       postBody.textContent = body;
-      mainContainer.append(postTitle);
-      mainContainer.append(postBody);
+
+      postMain.append(postTitle);
+      postMain.append(postBody);
+      mainContainer.append(postMain);
     });
   });
+
+// ---------------------
+// ---------------------
+//   MODAL POSTS
+
+const modalPosts = document.getElementById("modalPosts");
+
+postMain.addEventListener("click", function (e) {
+  console.log(e.target);
+  modalPosts.show();
+
+  //   fetchUsers();
+  //   console.log(usersArray);
+  //   console.log(postsArray[1].title);
+  //   comparePostUser();
+});
+
+console.log(postsArray);
+console.log(usersArray);
+
+// ------------------------------
+// ------------------------------
+// Functions
+
+function fetchUsers() {
+  const fetchUsers = fetch("http://localhost:3000/users");
+  fetchUsers
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      //   console.log(data);
+      data.map((user) => {
+        usersArray.push(user);
+      });
+    });
+}
+fetchUsers();
+
+function comparePostUser() {
+  //
+  postsArray.map((post) => {
+    usersArray.map((user) => {
+      if (post.id === user.id) {
+        console.log("asasa");
+      }
+    });
+  });
+}
