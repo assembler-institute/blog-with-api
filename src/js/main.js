2ff19a17a670dd5d9db08914cd41fbb43d11f76b
Sofia Romera <sofia-romera@outlook.com>
2022-05-15 23:57:30 +0200

    Added loadPosts function

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,36 @@
-/* <div class="card">
-  <div class="card-body">
-    This is some text within a card body.
-  </div>
-</div> */
+const usersURL = fetch("https://jsonplaceholder.typicode.com/users/");
+const postsURL = fetch("https://jsonplaceholder.typicode.com/posts/");
+const commentsURL = fetch("https://jsonplaceholder.typicode.com/comments/");
+
+function loadPosts(){
+    postsURL
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(post => {
+            const mainContainer = document.getElementById('cards-container');
+            const cardContainer = document.createElement('div');
+            cardContainer.classList.add('card', 'container__card--post');
+            const cardBody = document.createElement('div');
+            cardBody.classList.add('card-body');
+            const cardTitle = document.createElement('h5');
+            cardTitle.classList.add('card-title');
+            cardTitle.textContent = post.title.charAt(0).toUpperCase() + post.title.slice(1);
+            const cardText = document.createElement('p');
+            cardText.classList.add('card-text');
+            cardText.textContent = (post.body.charAt(0).toUpperCase() + post.body.slice(1)).slice(0, 80) + '...';
+            const link = document.createElement('a');
+            link.setAttribute('href', '#');
+            link.classList.add('card-link');
+            link.textContent = "read more...";
+
+            mainContainer.append(cardContainer);
+            cardContainer.append(cardBody);
+            cardBody.append(cardTitle, cardText, link);
+        });
+    })
+    .catch(error => {
+        return error
+    });
+}
+
+loadPosts();
