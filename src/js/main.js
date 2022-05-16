3ea9d47db494065f8cdb97db3b53a9b7615f4b8f
Sofia Romera <sofia-romera@outlook.com>
2022-05-16 23:36:49 +0200

    Added getComments function

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,15 @@
 
 const postsURL = fetch("https://jsonplaceholder.typicode.com/posts/");
-const commentsURL = fetch("https://jsonplaceholder.typicode.com/comments/");
 const contentBody = document.getElementById('modal__body-content');
-const contenTitle = document.getElementById('modal__title-content');
+const contentTitle = document.getElementById('modal__title-content');
 
 const userBodyPost = document.getElementById('user__body--post');
 const userNamePost = document.getElementById('user__name--post');
 const userEmailPost = document.getElementById('user__email--post');
+/*  */
+const commentBox = document.getElementById('collapseWidthExample');
+/*  */
+const collapseWidthExample = document.getElementById('collapseWidthExample'); //cajita pal comentario
 
 function loadPosts(){
     postsURL
@@ -19,20 +22,28 @@ function loadPosts(){
             const cardBody = document.createElement('div');
             cardBody.classList.add('card-body');
             const cardTitle = document.createElement('h5');
+            cardTitle.classList.add('card-title');
             const titleLink = document.createElement('a');
             titleLink.setAttribute('href','javaScript:void(0)')
             titleLink.setAttribute('data-bs-toggle', 'modal')
             titleLink.setAttribute('data-bs-target', '#exampleModal')
-            cardTitle.classList.add('card-title');
             titleLink.textContent = post.title.charAt(0).toUpperCase() + post.title.slice(1);
+            const delIcon = document.createElement('i');
             const cardText = document.createElement('p');
-            cardText.classList.add('card-text');
-            cardText.textContent = (post.body.charAt(0).toUpperCase() + post.body.slice(1)).slice(0, 80) + '...';
             const btnAdd = document.createElement('button');
             const btnDel = document.createElement('button');
             const addIcon = document.createElement('i');
-            const delIcon = document.createElement('i');
-            // const modalTitle = document.getElementById('modalTitle');
+            cardText.classList.add('card-text');
+            cardText.textContent = (post.body.charAt(0).toUpperCase() + post.body.slice(1)).slice(0, 80) + '...';
+            const boxUserComment = document.createElement('div');
+            boxUserComment.classList.add('card', 'card-body', 'toggle__comment--box');
+            
+            const paraTitle = document.createElement('p');
+            const paraBody = document.createElement('p');
+            const paraEmail = document.createElement('p');
+            paraTitle.classList.add('toggle__comment--para');
+            paraBody.classList.add('toggle__comment--para');
+            paraEmail.classList.add('toggle__comment--para');
             
             titleLink.setAttribute('id', post.userId);
 
@@ -43,11 +54,18 @@ function loadPosts(){
 
             cardTitle.addEventListener('click', () =>{
                 userBodyPost.textContent = (post.body.charAt(0).toUpperCase() + post.body.slice(1));
-                contenTitle.textContent = (post.title.charAt(0).toUpperCase() + post.title.slice(1));
-                userNamePost.textContent = getUser(titleLink.getAttribute('id'));
-                userEmailPost.textContent = getUser(titleLink.getAttribute('id'));
+                contentTitle.textContent = (post.title.charAt(0).toUpperCase() + post.title.slice(1));
+                let titleLinkId = titleLink.getAttribute('id')
+                userNamePost.textContent = getUser(titleLinkId);
+                userEmailPost.textContent = getUser(titleLinkId);
+                paraTitle.textContent = getComments(titleLinkId);
+                paraBody.textContent = getComments(titleLinkId);
+                paraEmail.textContent = getComments(titleLinkId);
+                
                 contentBody.append(userBodyPost, userNamePost, userEmailPost);
+                boxUserComment.append(paraTitle, paraBody, paraEmail);
             });
+
 
             cardTitle.append(titleLink)
             btnDel.append(delIcon);
@@ -69,12 +87,44 @@ function getUser(idUser){
         data.forEach(user => {
             userNamePost.textContent = 'Written by: ' + user.name;
             userEmailPost.textContent = 'Email: ' + user.email;
-            //console.log(user.name, user.email);
         });
     })
     .catch(error => {
         console.log(error);
     });
 };
+
+function getComments(idPost){
+    fetch(`http://localhost:3000/comments?postId=${idPost}`)
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(comment => {
+            //console.log('Title: ' + comment.name + ' ----- Comment: ' + comment.body + ' ------- Email: ' + comment.email);
+            paraTitle.textContent = comment.name;
+            paraBody.textContent = comment.body;
+            paraEmail.textContent = comment.email;
+            //comment.name.toUpperCase();
+        });
+    })
+    .catch(error => {
+        console.log(error);
+    });
+}
+//getComments(1);
+
+/*
+async function loadComments(idPost){
+    const response = await fetch(`http://localhost:3000/comments?postId=${idPost}`);
+    const comments = await response.json();
+    comments.forEach(comment => {
+        //console.log(comment.body);
+        //commentTitle.textContent = comment.title;
+        //commentBody.textContent = comment.body;
+        //commentUser.textContent = comment.email;
+    });
+} 
+*/
+//loadComments(1);
+
 
 loadPosts();
