83bc35601da52d1b2da225f575cb4f87047c0c24
oualidromli <oualid.romli@gmail.com>
2022-05-16 20:48:28 +0200

    DELETE fixed

--- src/main.js
+++ src/main.js
@@ -4,117 +4,105 @@ const postUrl = 'http://localhost:3000/posts';
 const commentUrl = 'http://localhost:3000/comments';
 
 fetch(postUrl)
-    .then((response) => response.json())
-    .then((data) => {
-        data.forEach((element) => {
-            let template = document.getElementById('post');
-            let divContainer =document.createElement('div');
+  .then((response) => response.json())
+  .then((data) => {
+    data.forEach((element) => {
+      let template = document.getElementById('post');
+      let divContainer = document.createElement('div');
 
-            const h3 = template.content.querySelector('h3');
-            h3.textContent = element.title;
+      const h3 = template.content.querySelector('h3');
+      h3.textContent = element.title;
 
-            const p = template.content.querySelector('p');
-            p.textContent = element.body;
+      const p = template.content.querySelector('p');
+      p.textContent = element.body;
 
-            template.content.querySelector('[role="button"]').dataset.id = element.id;
+      template.content.querySelector('[role="button"]').dataset.id = element.id;
 
-            template.content.querySelector('[role="btn-edit"]').dataset.id = element.id;
+      template.content.querySelector('[role="btn-edit"]').dataset.id =
+        element.id;
 
-            const clone = document.importNode(template.content, true);
-            container.appendChild(clone);
-            // template.append(divContainer);
-
-
-        });
-
-        // BUTTON EDIT
-        const editButton = document.querySelectorAll('[role="btn-edit"]');
-        editButton.forEach((button) => {
-            //MODAL POST API
-            button.addEventListener('click', () => {
-                const commentsDivs = document.querySelectorAll('.test');
-                commentsDivs.forEach((comment) => {
-                    comment.remove();
-                })
-                fetch('http://localhost:3000/posts/' + button.dataset.id)
-                .then((res) => res.json())
-                    .then((post) => {
-                        const title = document.querySelector('.modal-title')
-                        const body = document.querySelector('.modal-body')
-                        title.textContent = post.title;
-                        body.textContent = post.body;
-                        // MODAL USER API
-                        fetch('http://localhost:3000/users/' + post.userId)
-                            .then((res) => res.json())
-                            .then((user) => {
-                                const modalUser = document.querySelector('#userName');
-                                const modalEmail = document.querySelector('#email');
-                                modalUser.textContent = user.name;
-                                modalEmail.textContent = user.email;
-                            })
-                            const btnCmt = document.getElementById('btnLoadComments')
-                        btnCmt.addEventListener('click', () =>{
-                            fetch('http://localhost:3000/comments?postId=' + post.id)
-                            .then((res) => res.json())
-                            .then((comments) => {
-                            comments.forEach((comment) => {
-                                const commentsContainer = document.createElement('div');
-                                commentsContainer.classList.add("test", "p-3");
-                                console.log(commentsContainer);
-                                const commentsBody = document.createElement('p');
-                                const commentsName = document.createElement('h6');
-                                const commentsEmail = document.createElement('p');
-                                commentsName.textContent = comment.name;
-                                commentsBody.textContent = comment.body;
-                                commentsEmail.textContent = comment.email;
-                                commentsContainer.appendChild(commentsName);
-                                commentsContainer.appendChild(commentsBody);
-                                commentsContainer.appendChild(commentsEmail);
-                                btnCmt.insertAdjacentElement('afterEnd', commentsContainer);
-                            })
-                        })
-                    }
-                    )
-                })
-            });
-
-        });
+      const clone = document.importNode(template.content, true);
+      container.appendChild(clone);
+      // template.append(divContainer);
     });
 
-//REMOVE BTN AFTER CLICK
-const btnRemove = document.getElementById('btnLoadComments');
+    // BUTTON EDIT
+    const editButton = document.querySelectorAll('[role="btn-edit"]');
+    editButton.forEach((button) => {
+      //MODAL POST API
+      button.addEventListener('click', () => {
+        const commentsDivs = document.querySelectorAll('.test');
+        commentsDivs.forEach((comment) => {
+          comment.remove();
+        });
+        fetch('http://localhost:3000/posts/' + button.dataset.id)
+          .then((res) => res.json())
+          .then((post) => {
+            const title = document.querySelector('.modal-title');
+            const body = document.querySelector('.modal-body');
+            title.textContent = post.title;
+            body.textContent = post.body;
+            // MODAL USER API
+            fetch('http://localhost:3000/users/' + post.userId)
+              .then((res) => res.json())
+              .then((user) => {
+                const modalUser = document.querySelector('#userName');
+                const modalEmail = document.querySelector('#email');
+                modalUser.textContent = user.name;
+                modalEmail.textContent = user.email;
+              });
+            const btnCmt = document.getElementById('btnLoadComments');
+            btnCmt.addEventListener('click', () => {
+              fetch('http://localhost:3000/comments?postId=' + post.id)
+                .then((res) => res.json())
+                .then((comments) => {
+                  comments.forEach((comment) => {
+                    const commentsContainer = document.createElement('div');
+                    commentsContainer.classList.add('test', 'p-3');
+                    console.log(commentsContainer);
+                    const commentsBody = document.createElement('p');
+                    const commentsName = document.createElement('h6');
+                    const commentsEmail = document.createElement('p');
+                    commentsName.textContent = comment.name;
+                    commentsBody.textContent = comment.body;
+                    commentsEmail.textContent = comment.email;
+                    commentsContainer.appendChild(commentsName);
+                    commentsContainer.appendChild(commentsBody);
+                    commentsContainer.appendChild(commentsEmail);
+                    btnCmt.insertAdjacentElement('afterEnd', commentsContainer);
+                  });
+                });
+            });
+          });
+      });
+    });
+    const deleteButtons = document.querySelectorAll('[role="button"]');
 
-
-// BUTTON DELETE
-const deleteButtons = document.querySelectorAll('[role="button"]');
-// const deleteButtons = document.getElementById('buttonDelete');
-console.log(deleteButtons);
-
-deleteButtons.forEach((element) => {
-    element.addEventListener('click', () => {
+    deleteButtons.forEach((element) => {
+      element.addEventListener('click', () => {
         // element.parentNode.parentNode.remove()}
         fetch('http://localhost:3000/posts/' + element.dataset.id, {
-            method: 'DELETE',
+          method: 'DELETE',
         });
+      });
     });
-});
+  });
 
-
+//REMOVE BTN AFTER CLICK
+// BUTTON DELETE
 
 //** FUNCTION THAT GET POSTID AND ID FROM DATA BASE **//
-let idNumber = [];
-let postIdNumber = [];
+// let idNumber = [];
+// let postIdNumber = [];
 
 //This function get ID from current post comment
-function getId() {
-    fetch(postUrl)
-        .then((response) => response.json())
-        .then((data) => {
-            data.forEach((idValue) =>
-                idNumber.push(idValue.id),
-            )
-        })
-}
+// function getId() {
+//   fetch(postUrl)
+//     .then((response) => response.json())
+//     .then((data) => {
+//       data.forEach((idValue) => idNumber.push(idValue.id));
+//     });
+// }
 
 //This function get postID from the user which comment the post
 // function getPostId() {
@@ -126,7 +114,6 @@ function getId() {
 //             })
 //         })
 // }
-
 
 // const btnLoadComments = document.getElementById('btnLoadComments');
 // btnLoadComments.addEventListener('click', loadComment);
@@ -164,8 +151,6 @@ function getId() {
 //             })
 //         })
 // }
-
-
 
 // console.log(postIdNumber === idNumber.length);
 // function compareId() {
